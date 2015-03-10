84aa59274865dd6cacedeecbc88a206801206b43
Yegor Bugayenko <yegor@tpc2.com>
2015-03-09 17:35:00 -0700

    -Refactoring: 	1. Release all unneeded resources to prevent the memory leak 	2. Make the class as singleton 	3. Make the class full thread safe(the caller unneeded to worry about call the parser in thread safe context), in the old implementation we couldn't make the class as thread safe without make the call for setFile and getContent in synchronized block

--- Parser.java
+++ Parser.java
@@ -1,3 +1,5 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -6,37 +8,85 @@ import java.io.IOException;
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+	/**
+	 *  Create one instance in each JVM
+	 */
+	private static Parser instance = new Parser();
+	
+	/**
+	 * Make the constructor as private
+	 */
+	private Parser () {
+	}
+	
+	/**
+	 * Get instance to call the others methods
+	 * 
+	 * @return Parser instance
+	 */
+	public static Parser getInstance() {
+		return instance;
+	}
+	
+	/** 
+	 * Get the content from the passed File
+	 * 
+	 * @param file	File that needed to scan
+	 * @return		The file content
+	 * 
+	 * @throws IOException
+	 */
+	public String getContent(File file) throws IOException {
+		try (BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(file))) {
+			StringBuilder output = new StringBuilder();
+		    int data;
+		    
+		    while ((data = bufferedInputStream.read()) > 0) {
+		      output.append((char) data);
+		    }
+		    
+		    return output.toString();
+		}
+	}
+	
+	/** 
+	 * Get the content without unicode characters from the passed File
+	 * 
+	 * @param file	File that needed to scan
+	 * @return		The file content
+	 * 
+	 * @throws IOException
+	 */
+	public String getContentWithoutUnicode(File file) throws IOException {
+		try (BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(file))) {
+			StringBuilder output = new StringBuilder();
+		    int data;
+		    
+		    while ((data = bufferedInputStream.read()) > 0) {
+		      if (data < 0x80) {
+		        output.append((char) data);
+		      }
+		    }
+		    
+		    return output.toString();
+		}
+	}
+	
+	/**
+	 * Save the passed content to passed file
+	 * 
+	 * @param content	The content
+	 * @param file		The target file
+	 * 
+	 * @throws IOException
+	 */
+	public void saveContent(String content, File file) throws IOException {
+		try(BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(file))) {
+			for (int i = 0; i < content.length(); i += 1) {
+				bufferedOutputStream.write(content.charAt(i));
+			}
+		    
+			bufferedOutputStream.flush();
+	    }
+	}
 }
