0a774562f37b7c832bb83226d8f91778cb36bd5b
KevBurnsJr <kevburnsjr@gmail.com>
2015-03-11 13:49:46 -0400

    Reference columns by name rather than order

--- Document.php
+++ Document.php
@@ -14,13 +14,13 @@ class Document {
     public function getTitle() {
         $db = Database::getInstance();
         $row = $db->query('SELECT * FROM document WHERE name = "' . $this->name . '" LIMIT 1');
-        return $row[3]; // third column in a row
+        return $doc['title'];
     }
 
     public function getContent() {
         $db = Database::getInstance();
         $row = $db->query('SELECT * FROM document WHERE name = "' . $this->name . '" LIMIT 1');
-        return $row[6]; // sixth column in a row
+        return $doc['content'];
     }
 
     public static function getAllDocuments() {
