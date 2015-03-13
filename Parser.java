6de25f9e780226d9fc9f4d9b74edad383422cda7
rajeev bahuguna <rbahuguna@github.com>
2015-03-13 15:39:57 +0530

    Closed io streams after use, added javadoc, formatted code

--- Parser.java
+++ Parser.java
@@ -2,17 +2,34 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
   private File file;
+
+  /**
+   * 
+   * @param f
+   */
   public synchronized void setFile(File f) {
     file = f;
   }
+
+  /**
+   * 
+   * @return
+   */
   public synchronized File getFile() {
     return file;
   }
+
+  /**
+   * 
+   * @return
+   * @throws IOException
+   */
   public String getContent() throws IOException {
     FileInputStream i = new FileInputStream(file);
     String output = "";
@@ -20,8 +37,15 @@ public class Parser {
     while ((data = i.read()) > 0) {
       output += (char) data;
     }
+    i.close();
     return output;
   }
+
+  /**
+   * 
+   * @return
+   * @throws IOException
+   */
   public String getContentWithoutUnicode() throws IOException {
     FileInputStream i = new FileInputStream(file);
     String output = "";
@@ -31,12 +55,20 @@ public class Parser {
         output += (char) data;
       }
     }
+    i.close();
     return output;
   }
+
+  /**
+   * 
+   * @param content
+   * @throws IOException
+   */
   public void saveContent(String content) throws IOException {
     FileOutputStream o = new FileOutputStream(file);
     for (int i = 0; i < content.length(); i += 1) {
       o.write(content.charAt(i));
     }
+    o.close();
   }
 }
