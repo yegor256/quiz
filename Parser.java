4de9d4789aff30d94695ade4a65be813a30a370a
mda <dima.molotchko@gmail.com>
2015-03-13 21:59:43 +0300

    Fix string performance issue Fix resource leakage Fix file resource synchronization issue Fix file access performance issue

--- Parser.java
+++ Parser.java
@@ -1,12 +1,18 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 /**
  * This class is thread safe.
  */
+// TODO: split File setter/getter and read/write synchronization
 public class Parser {
-  private File file;
+  private static final int BUFFSIZE = 1024*16;
+private File file;
   public synchronized void setFile(File f) {
     file = f;
   }
@@ -21,21 +27,25 @@ public class Parser {
   }
   
   synchronized private String getContentInternal(boolean all) throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if( all || data < 0x80 ) {
-        output += (char) data;
-      }
+    InputStream is = new BufferedInputStream(new FileInputStream(file));
+    StringBuilder sb = new StringBuilder();
+    byte buff[] = new byte[BUFFSIZE];
+    int rc;
+    while( (rc=is.read(buff, 0, BUFFSIZE)) != -1 ){
+    	for(int i=0;  i<rc;  ++i ){
+    	      if( all || buff[i] < 0x80 ) {
+    	    	  sb.append((char) buff[i]);
+    	      }
+    	}
     }
-    return output;
+    is.close();
+    return sb.toString();
   }
   
   synchronized public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
+    OutputStream o = new BufferedOutputStream(new FileOutputStream(file));
+    o.write(content.getBytes());
+    o.close();
   }
 }
+
