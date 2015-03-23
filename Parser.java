4cc3c64032f36ce7292a9562a4ae18d56724f1b8
fjleon <fjleontorrijo@gmail.com>
2015-03-23 17:07:43 +0100

    Update Parser.java
    My corrections on Parser.java

--- Parser.java
+++ Parser.java
@@ -1,42 +1,96 @@
+/**
+ * Copyright (c) 2011-2014, Qulice.com
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: no conditions.
+ */
+package org.company.teamed;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
+ * @author Francisco Javier (fjleontorrijo@gmail.com)
+ * @version $Id$
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+    /**
+     * Constant for Unicode characters.
+     */
+    private static final int UNICODE = 0x80;
+
+    /**
+     * File descriptor.
+     */
+    private final transient File file;
+
+    /**
+     * Constructor with file param.
+     * @param parseable File to parse.
+     */
+    public Parser(final File parseable) {
+        this.file = parseable;
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    /**
+     * Read file content.
+     * @param unicode True for read content with Unicode, false otherwise.
+     * @return File content.
+     * @throws IOException If I/O error occurs.
+     */
+    public final StringBuffer getContent(final boolean unicode)
+            throws IOException {
+        final StringBuffer output = new StringBuffer();
+        synchronized (this.file) {
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(this.file);
+                if (unicode) {
+                    for (int data = fis.read(); data > 0; data = fis.read()) {
+                        output.append((char) data);
+                    }
+                } else {
+                    for (int data = fis.read(); data > 0; data = fis.read()) {
+                        if (data < Parser.UNICODE) {
+                            output.append((char) data);
+                        }
+                    }
+                }
+            } finally {
+                if (fis != null) {
+                    fis.close();
+                }
+            }
+        }
+        return output;
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    /**
+     * Save content to file.
+     * @param content Content to save.
+     * @throws IOException If I/O occurs.
+     */
+    public final void saveContent(final String content) throws IOException {
+        synchronized (this.file) {
+            if (content != null) {
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream(this.file);
+                    for (int pos = 0; pos < content.length(); pos += 1) {
+                        fos.write(content.charAt(pos));
+                    }
+                } finally {
+                    if (fos != null) {
+                        fos.close();
+                    }
+                }
+            }
+        }
     }
-  }
+
 }
