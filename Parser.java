5c0cb69a6b04b856f669410bc49b638863c89f7a
YamStranger <yamstranger@bk.ru>
2015-04-01 15:45:49 +0300

    Test commit

--- Parser.java
+++ Parser.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.IOException; 
 /**
  * This class is thread safe.
  */
