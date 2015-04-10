bfe3ad3e1e8983e10ecb69411972e4f3af6ff404
Rui C. <chenrui1988@gmail.com>
2015-04-10 14:48:29 +0800

    refactor Parser class

--- Parser.java
+++ Parser.java
@@ -1,42 +1,70 @@
+package com.isoftstone.web;
+
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+    public String getContent(File file) {
+        FileInputStream i = null;
+        StringBuffer output = new StringBuffer();
+        try {
+            i = new FileInputStream(file);
+            byte[] buf = new byte[1024];
+            int data = 0;
+            while ((data = i.read(buf)) > 0) {
+                output.append(new String(buf, 0, data));
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            this.closeQuietly(i);
+        }
+        return output.toString();
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    public String getContentWithoutUnicode(File file) throws IOException {
+        FileInputStream i = null;
+        StringBuffer output = new StringBuffer();
+        try {
+            i = new FileInputStream(file);
+            int data = 0;
+            while ((data = i.read()) > 0) {
+                if(data < 0x80) {
+                    output.append((char)data);
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            this.closeQuietly(i);
+        }
+        return output.toString();
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+    
+    private void closeQuietly(InputStream is) {
+        try {
+            is.close(); 
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
     }
-  }
+
+    public void saveContent(String content, File file) throws IOException {
+        BufferedWriter out = null;
+        try {
+            out = new BufferedWriter(new FileWriter(file)); 
+            out.write(content);
+        } finally {
+            out.close();
+        }
+    }
 }
