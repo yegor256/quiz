37aea0926ec0f18b63574966d30bf99697e85799
akurutin <akurutin@gmail.com>
2015-04-10 10:49:31 +0300

    Update Parser.java
    oDesk author: Alexandr Kurutin

--- Parser.java
+++ Parser.java
@@ -2,41 +2,38 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+    public static String getContent(File file, boolean withoutUnicode) throws IOException {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            String output = "";
+            int data;
+            while ((data = fileInputStream.read()) > 0) {
+                if (withoutUnicode && data < 0x80) {
+                    output += (char) data;
+                } else if (!withoutUnicode) {
+                    output += (char) data;
+                }
+            }
+            return output;
+        } catch (IOException e) {
+            throw new IOException("getContentException", e);
+        }
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    public static File saveContent(String fn, String content) throws IOException {
+        File file = new File(fn);
+        try (FileOutputStream o = new FileOutputStream(file)) {
+            for (int i = 0; i < content.length(); i += 1) {
+                o.write(content.charAt(i));
+            }
+            return file;
+        } catch (IOException e) {
+            throw new IOException("saveContentException", e);
+        }
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
 }
