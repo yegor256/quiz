e58b6a2c95c2b61a8b4374e5c4f68c5e0a01f0ed
Abhishek Kejriwal <akejriwal21@yahoo.com>
2015-04-11 10:23:33 +0530

    Please enter the commit message for your changes. Lines starting commiting changes as requested

--- Parser.java
+++ Parser.java
@@ -2,41 +2,56 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
  */
-public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+public class Parser 
+{
+	private File file;
+
+	public synchronized void setFile(File file) 
+	{
+		this.file = file;
+	}
+
+	public synchronized File getFile() 
+	{
+		return file;
+	}
+
+	public synchronized String getContent() throws IOException 
+	{
+		FileInputStream fileInputStream = new FileInputStream(file);
+		String output = "";
+		int data;
+		while ((data = fileInputStream.read()) > 0) {
+			output += (char) data;
+		}
+		fileInputStream.close();
+		return output;
+	}
+
+	public String getContentWithoutUnicode() throws IOException 
+	{
+		FileInputStream fileInputStream = new FileInputStream(file);
+		String output = "";
+		int data;
+		while ((data = fileInputStream.read()) > 0) {
+			if (data < 0x80) {
+				output += (char) data;
+			}
+		}
+		fileInputStream.close();
+		return output;
+	}
+
+	public synchronized void saveContent(String content) throws IOException 
+	{
+		FileOutputStream fileOutputStream = new FileOutputStream(file);
+		for (int i = 0; i < content.length(); i += 1) {
+			fileOutputStream.write(content.charAt(i));
+		}
+		fileOutputStream.close();
+	}
 }
