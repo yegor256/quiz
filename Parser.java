c7a250cd7b0800ccda8033bdfbf2b719595efca0
alex <alexsmenyuk88@gmail.com>
2015-04-17 19:41:26 +0200

    Fix issues in Parser

--- Parser.java
+++ Parser.java
@@ -1,42 +1,82 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+    private File file;
+
+    private static final String encoding = "UTF-16";
+
+    public Parser(File f) {
+        this.file = file;
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    public StringBuffer getContent() {
+        BufferedReader i = null;
+        StringBuffer output = new StringBuffer("");
+        String data;
+        try {
+            i = new BufferedReader(
+                    new InputStreamReader(new FileInputStream(file), encoding));
+            while ((data = i.readLine()) != null) {
+                output.append(data);
+            }
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (i != null)
+                    i.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return output;
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    public StringBuffer getContentWithoutUnicode() throws IOException {
+        FileInputStream i = null;
+        StringBuffer output = new StringBuffer("");
+        int data;
+        try {
+            i = new FileInputStream(file);
+            while ((data = i.read()) != -1) {
+                if (data < 0x80) {
+                    output.append(Integer.toString(data));
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (i != null)
+                    i.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return output;
     }
-  }
+
+    public synchronized void saveContent(String content) throws IOException {
+        Writer o = null;
+        try {
+            o = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(file)));
+            o.write(content);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                o.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
