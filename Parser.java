a2d76cd871b494114996fd1526eaa5308edcf0ca
Aleksandr Nikiforov <aleksandr.nikiforov@transferwise.com>
2015-04-21 22:28:49 +0300

    Some refactorings

--- Parser.java
+++ Parser.java
@@ -2,41 +2,65 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+	private File file;
+
+	public synchronized void setFile(File f) {
+		file = f;
+	}
+
+	public File getFile() {
+		return file;
+	}
+
+	public String getContent() throws IOException {
+		return getContent(true);
+	}
+
+	public String getContentWithoutUnicode() throws IOException {
+		return getContent(false);
+	}
+
+	private String getContent(boolean includeUnicode) throws IOException {
+		FileInputStream inputStream = null;
+		try {
+			inputStream = new FileInputStream(getFile());
+			StringBuilder output = new StringBuilder();
+			int data;
+			while ((data = inputStream.read()) > 0) {
+				if (!isUnicode(data) || includeUnicode) {
+					char c = (char) data;
+					output.append(c);
+				}
+			}
+			return output.toString();
+		} finally {
+			if (inputStream != null) {
+				inputStream.close();
+			}
+		}
+	}
+
+	private boolean isUnicode(int data) {
+		return data < 0x80;
+	}
+
+	public synchronized void saveContent(String content) throws IOException {
+		FileOutputStream outputStream = null;
+		try {
+			outputStream = new FileOutputStream(getFile());
+			for (int i = 0; i < content.length(); i += 1) {
+				outputStream.write(content.charAt(i));
+			}
+		} finally {
+			if (outputStream != null) {
+				outputStream.close();
+			}
+		}
+	}
+	
 }
