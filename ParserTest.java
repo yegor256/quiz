44ce6f8dd337cb67c4b140a9a332fc89251ec618
ro <robertogarridomartin@gmail.com>
2015-04-22 10:40:18 +0200

    Added a dummy test parser

---
+++ ParserTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by ro on 22/4/15.
+ */
+public class ParserTest {
+
+    @org.junit.Test
+    public void testGetContent() throws Exception {
+        assert(true);
+    }
+
+    @org.junit.Test
+    public void testGetContentWithoutUnicode() throws Exception {
+        assert(true);
+    }
+
+    @org.junit.Test
+    public void testSaveContent() throws Exception {
+        assert(true);
+    }
+}
