71a5a6e85efb1a44e8548461c15845defcde0706
JBuriro <junaid.92@hotmail.com>
2015-04-22 15:37:50 +0500

    Added recommendations

--- Parser.java
+++ Parser.java
@@ -13,6 +13,21 @@ public class Parser {
   public synchronized File getFile() {
     return file;
   }
+  /*
+  ********************* 1st Recommendation ************
+  Instead of using String, we have to use StringBuilder. As String is immutable object, lot of String objects would 
+  
+     be created, which is waste of resources. Furthermore, as program is thread Safe we would use StringBuilder not StringBuffer
+     
+     Format : StringBuilder object = new StringBuilder();
+              object.append("some string ");
+     */ 
+     
+     
+       /*
+  ********************* 2nd Recommendation ************
+      Close the streams (input and output) after their use
+     */ 
   public String getContent() throws IOException {
     FileInputStream i = new FileInputStream(file);
     String output = "";
