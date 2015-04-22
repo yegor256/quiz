03b5bac98d6597ca801b36259177ba49918700ac
Serge Rogatch <serge.rogatch@gmail.com>
2015-04-22 12:43:12 +0300

    First step in refactoring of Parser class.

--- Parser.java
+++ Parser.java
@@ -1,42 +1,62 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+
 /**
- * This class is thread safe.
+ * This class is thread safe in the meaning that the data stored in it is thread-safe.
+ * However, problems can occur if multiple threads start to read and write the file
+ *   simultaneously (calling both getContent*() and saveContent()). Safety for such
+ *   scenarios is a matter of further refactoring.
  */
 public class Parser {
   private File file;
+
   public synchronized void setFile(File f) {
     file = f;
   }
   public synchronized File getFile() {
     return file;
   }
+  
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+    BufferedReader br = new BufferedReader( new FileReader( getFile() ) );
+    try {
+      StringBuilder output = new StringBuilder();
+      while(true) {
+        int data = br.read();
+        if( data < 0 ) {
+          break;
+        }
+        output.append( (char)data );
+      }
+      return output.toString();
+    } finally {
+      br.close();
     }
-    return output;
   }
+  
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
+    String content = getContent();
+    StringBuilder output = new StringBuilder();
+    for( int i=0; i<content.length(); i++ ) {
+      char ch = content.charAt(i);
+      if( (int)ch < 0x80 ) {
+        output.append(ch);
       }
     }
-    return output;
+    return output.toString();
   }
+  
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+    BufferedWriter bw = new BufferedWriter( new FileWriter( getFile() ));
+    try {
+      bw.write(content);
+    } finally {
+      bw.close();
     }
   }
+  
 }
