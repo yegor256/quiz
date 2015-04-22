23bfce5c95d010be1e54dffb973f9865951a57e3
siva636 <siva636@gmail.com>
2015-04-22 14:37:59 +0530

    Update Parser.java
    Refactored.

--- Parser.java
+++ Parser.java
@@ -2,41 +2,68 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+    private static final Logger LOG = Logger.getLogger(Parser.class.getName());
+
+    private File file;
+
+    public synchronized void setFile(File f) {
+        file = f;
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    public synchronized File getFile() {
+        return file;
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    public String getContent() {
+
+        String output = "";
+        int data;
+
+        try {
+            FileInputStream i = new FileInputStream(file);
+            while ((data = i.read()) > 0) {
+                output += (char) data;
+            }
+        } catch (IOException iOException) {
+            LOG.log(Level.SEVERE, "Cannot read file");
+        }
+        
+        return output;
     }
-  }
+
+    public String getContentWithoutUnicode() {
+
+        String output = "";
+        int data;
+        
+        try {
+            FileInputStream i = new FileInputStream(file);
+            while ((data = i.read()) > 0) {
+                if (data < 0x80) {
+                    output += (char) data;
+                }
+            }
+        } catch (IOException iOException) {
+            LOG.log(Level.SEVERE, "Cannot read file");
+        }
+        
+        return output;
+    }
+
+    public void saveContent(String content) throws IOException {
+        FileOutputStream o = new FileOutputStream(file);
+        for (int i = 0; i < content.length(); i += 1) {
+            o.write(content.charAt(i));
+        }
+    }
 }
+
+ 
