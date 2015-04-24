fd8279a31aa2f7c4245576d46d7f459ccac17217
kadekunl <kadekunl@users.noreply.github.com>
2015-04-24 15:28:49 +0100

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -1,3 +1,5 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -13,30 +15,42 @@ public class Parser {
   public synchronized File getFile() {
     return file;
   }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
+  public synchronized String getContent() throws IOException {
+    if(!(file != null && file.isFile())) throw new IOException("File error");
+    
+    BufferedInputStream i = new BufferedInputStream (new FileInputStream(file));
+    StringBuilder output = new StringBuilder();
+    int data = 0;
     while ((data = i.read()) > 0) {
-      output += (char) data;
+      output.append((char) data);
     }
-    return output;
+    
+    i.close();
+    return output.toString();
   }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
+  public synchronized String getContentWithoutUnicode() throws IOException {
+    if(!(file != null && file.isFile())) throw new IOException("File error");
+    
+    BufferedInputStream i = new BufferedInputStream(new FileInputStream(file));
+    StringBuilder output = new StringBuilder();
+    int data = 0;
     while ((data = i.read()) > 0) {
       if (data < 0x80) {
-        output += (char) data;
+        output.append((char) data);
       }
     }
-    return output;
+    
+    i.close();
+    return output.toString();
   }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
+  public synchronized void saveContent(String content) throws IOException {
+    if(!(file != null && file.isFile())) throw new IOException("File error");
+    
+    BufferedOutputStream o = new BufferedOutputStream(new FileOutputStream(file));
     for (int i = 0; i < content.length(); i += 1) {
       o.write(content.charAt(i));
     }
+    
+    o.close();
   }
 }
