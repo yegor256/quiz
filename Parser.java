6e7cb0a4f379939a53db6aac5eb3ff7edf5db8a4
Nikola Radovanovic <nikoladsp@gmail.com>
2015-04-27 00:46:10 +0200

    Senior C++ programmers oDesk job application
    Signed-off-by: Nikola Radovanovic <nikoladsp@gmail.com>

--- Parser.java
+++ Parser.java
@@ -1,42 +1,45 @@
+
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+    private final File file;
+    private final FileInputStream fileStream;
+
+    public Parser(final String filePath) throws FileNotFoundException {
+        this.file = new File(filePath);
+        this.fileStream = new FileInputStream(this.file);
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    public synchronized String getContent() throws IOException {
+        final StringBuilder output = new StringBuilder();
+        int data;
+        while ((data = fileStream.read()) > 0) {
+            output.append((char) data);
+        }
+        return output.toString();
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    public synchronized String getContentWithoutUnicode() throws IOException {
+        final StringBuilder output = new StringBuilder();
+        int data;
+        while ((data = fileStream.read()) > 0) {
+            if (data < 0x80) {
+                output.append((char) data);
+            }
+        }
+        return output.toString();
     }
-  }
+
+    public synchronized void saveContent(final String content) throws IOException {
+        final FileOutputStream o = new FileOutputStream(file);
+        o.write(content.getBytes());
+    }
 }
