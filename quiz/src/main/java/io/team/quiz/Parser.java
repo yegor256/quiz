f4e8bc186534c9851fc8b939c601957b01e0411a
Gadi <Gadi@LenovoWin8>
2015-04-26 12:06:46 +0300

    use modern input string and builders

--- quiz/src/main/java/io/team/quiz/Parser.java
+++ quiz/src/main/java/io/team/quiz/Parser.java
@@ -1,11 +1,22 @@
 package io.team.quiz;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CoderResult;
+import java.nio.charset.CodingErrorAction;
+import java.nio.charset.StandardCharsets;
 
 /**
  * This class is thread safe.
@@ -23,30 +34,38 @@ public class Parser {
     }
 
     public String getContent() throws IOException {
-        FileInputStream i = new FileInputStream(file);
-        String output = "";
-        int data;
-        while ((data = i.read()) > 0) {
-            output += (char) data;
+        final StringBuilder sb = new StringBuilder();
+        InputStream is = new FileInputStream(file);
+        final InputStreamReader isr = new InputStreamReader(is);
+        final BufferedReader br = new BufferedReader(isr);
+        String line = "";
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
         }
-        return output;
+        return sb.toString();
     }
 
     public String getContentWithoutUnicode() throws IOException {
-        FileInputStream i = new FileInputStream(file);
-        String output = "";
-        int data;
-        while ((data = i.read()) > 0) {
-            if (data < 0x80) {
-                output += (char) data;
-            }
+        final StringBuilder sb = new StringBuilder();
+        InputStream is = new FileInputStream(file);
+        
+        // adding a decoder to igonore unmapped chars (not unicode UTF-8)
+        CharsetDecoder decoder = StandardCharsets.UTF_8.newDecoder();
+        decoder.onMalformedInput(CodingErrorAction.IGNORE);
+
+        final InputStreamReader isr = new InputStreamReader(is, decoder);
+        final BufferedReader br = new BufferedReader(isr);
+        String line = "";
+        while ((line = br.readLine()) != null) {
+            //line.replace();
+            sb.append(line);
         }
-        return output;
+        return sb.toString();
     }
 
     public void saveContent(String content) throws IOException {
         try (BufferedWriter output = new BufferedWriter(new FileWriter(file))) {
             output.write(content);
-        }    
+        }
     }
 }
