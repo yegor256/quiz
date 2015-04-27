fb03c31fd0c4d7e9b2a2795fd5688d55f060c60b
LiuMaoyang <wuzi.xiao@gmail.com>
2015-04-28 11:22:18 +1200

    fix some design and performance problems

--- Parser.java
+++ Parser.java
@@ -6,37 +6,47 @@ import java.io.IOException;
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+    final private File file;
+
+    public Parser(String filename) throws FileNotFoundException {
+        this.file = new File(filename);
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+
+    public synchronized String getContent() throws IOException {
+        try (final FileInputStream i = new FileInputStream(file)) {
+            StringBuilder output = new StringBuilder();
+            int data;
+            byte buf[] = new byte[1024];
+            while ((data = i.read(buf)) > 0) {
+                output.append(buf);
+            }
+            return output;
+        }
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    public synchronized String getContentWithoutUnicode() throws IOException {
+        try(final FileInputStream i = new FileInputStream(file)) {
+            StringBuilder readBuffer = new StringBuilder();
+            int data;
+            byte buf[] = new byte[1024];
+            while ((data = i.read(buf)) > 0) {
+                readBuffer.append(buf);
+            }
+
+            StringBuilder output = new StringBuilder();
+            for (int index=0; index < readBuffer.length(); index++) {
+                if(readBuffer.charAt(index) < 0x80) {
+                    output.append(readBuffer.charAt(index));
+                }
+            }
+            return output.toString();
+        }
     }
-  }
+    public synchronized void saveContent(String content) throws IOException {
+        try (final FileOutputStream out = new FileOutputStream(file)) {
+            out.write(content.getBytes());
+        }
+
+    }
 }
