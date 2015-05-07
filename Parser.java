1b5c9f03bc6de5869441c57a3f51c6f7aa8f57d8
maulikupatel <maulikupatel@gmail.com>
2015-05-07 14:50:28 +0530

    Refactoring to avoid code duplication

--- Parser.java
+++ Parser.java
@@ -7,34 +7,26 @@ import java.io.IOException;
  */
 public class Parser {
   private File file;
+  enum UTF8_ENCODE {YES, NO };
   public synchronized void setFile(File f) {
     file = f;
   }
   public synchronized File getFile() {
     return file;
   }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+  public String getContent(UTF8_ENCODE encode) throws IOException {
+    FileInputStream i = new FileInputStream(getFile());
     String output = "";
     int data;
     while ((data = i.read()) > 0) {
+      if (encode = UTF8_ENCODE.FALSE || data < 0x80)
       output += (char) data;
     }
     return output;
   }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
+
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
+    FileOutputStream o = new FileOutputStream(getFile());
     for (int i = 0; i < content.length(); i += 1) {
       o.write(content.charAt(i));
     }
