5945b575b38b117f482def2135ef3abc00781bbe
Miguelángel <miguelangelcg25@gmail.com>
2015-05-11 18:14:12 -0500

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -1,9 +1,7 @@
 package quiz;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
