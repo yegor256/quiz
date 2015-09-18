1b4712e4216b9c383038b0dd29008724d02d910b
petabytedata <petabyte.data@gmail.com>
2015-09-18 12:54:50 +0530

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -6,37 +7,53 @@ import java.io.IOException;
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+
+	private final File file;
+
+	public Parser(final File file){
+		this.file = file;  
+	}
+
+	public File getFile() {
+		return file;
+	}
+
+	public String getContent() throws IOException {
+		String output = "";
+		//TODO: Use BufferedStream
+		try (FileInputStream inputStream = new FileInputStream(file)) {
+			int data;
+			while ((data = inputStream.read()) > 0) {
+				output += (char) data;
+			}
+		} catch (IOException ex) {
+			//TODO:Exception handling...
+		}
+		return output;
+	}
+	public String getContentWithoutUnicode() throws IOException {
+		//TODO: Use BufferedStream
+		String output = "";
+		try(FileInputStream inputStream = new FileInputStream(file)){
+			int data;
+			while ((data = inputStream.read()) > 0) {
+				if (data < 0x80) {
+					output += (char) data;
+				}
+			}
+		}catch(IOException ex){
+			//TODO:Exception handling...
+		}
+		return output;
+	}
+	public void saveContent(final String content) throws IOException {
+		//TODO: Use BufferedStream
+		try(FileOutputStream outStream = new FileOutputStream(file)){
+			for (int index = 0; index < content.length(); index += 1) {
+				outStream.write(content.charAt(index));
+			}
+		}catch(IOException ex){
+			//TODO:Exception handling...
+		}
+	}
 }
