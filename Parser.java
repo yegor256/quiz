314c7f6cfc201ff13dd28a3f0d9f778c1e75727b
tushardhole <tushardhole@hotmail.com>
2015-09-19 17:56:48 +0530

    - Changed string to string buffer for writing data - Added synchronizations to some method - Added finally block to close all the streams - Few Variable renaming - Added constructor with null validations - Complete formatting is changed, this is my bad, I should not change complete formatting, still going with this as I don not know which formatted was used for original file - Spent around 15 minutes

--- Parser.java
+++ Parser.java
@@ -1,42 +1,82 @@
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+	private File file;
+
+	public Parser(File fileToParse) {
+		if (fileToParse == null) {
+			throw new IllegalArgumentException("File to parse can not be null");
+		}
+		this.file = fileToParse;
+	}
+
+	public synchronized void setFile(File f) {
+		file = f;
+	}
+
+	public synchronized File getFile() {
+		return file;
+	}
+
+	public synchronized String getContent() throws IOException {
+		FileInputStream readStream = null;
+		try {
+			readStream = new FileInputStream(file);
+			StringBuffer output = new StringBuffer();
+			int data;
+			while ((data = readStream.read()) > 0) {
+				output.append(data);
+			}
+
+			return output.toString();
+
+		} finally {
+			if (readStream != null) {
+				readStream.close();
+			}
+		}
+	}
+
+	public synchronized String getContentWithoutUnicode() throws IOException {
+		FileInputStream writeStream = null;
+		try {
+			writeStream = new FileInputStream(file);
+			StringBuffer output = new StringBuffer();
+			int data;
+			while ((data = writeStream.read()) > 0) {
+				if (data < 0x80) {
+					output.append(data);
+				}
+			}
+			return output.toString();
+		} finally {
+			if (writeStream != null) {
+				writeStream.close();
+			}
+		}
+	}
+
+	public synchronized void saveContent(String content) throws IOException {
+		FileOutputStream writeStream = null;
+		BufferedOutputStream bufferedWriteStream = null;
+		try {
+			if (content != null && content.trim().length() > 0) {
+				writeStream = new FileOutputStream(file);
+				bufferedWriteStream = new BufferedOutputStream(writeStream);
+				bufferedWriteStream.write(content.getBytes());
+			}
+		} finally {
+			if (writeStream != null && bufferedWriteStream != null) {
+				writeStream.close();
+				bufferedWriteStream.close();
+			}
+		}
+	}
 }
