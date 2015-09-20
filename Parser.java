4435a546bb6df17e0c1e6275ec7ed988dfde3b0c
Daniyal Shahid <daniyal36@gmail.com>
2015-09-20 23:39:07 +0500

    Updated Parser.java
    - Combined FileInputStream and output variable into single variable since its being initiated twice in separate functions
    - Since the class is thread safe added sync
    - pulled  inputStream.read out of the loop to improve code readability.

--- Parser.java
+++ Parser.java
@@ -7,33 +7,32 @@ import java.io.IOException;
  */
 public class Parser {
   private File file;
-  public synchronized void setFile(File f) {
+  private FileInputStream inputStream;
+  String output = "";
+  public synchronized void setFile(File f) throws IOException {
     file = f;
+    inputStream = new FileInputStream(file);
   }
   public synchronized File getFile() {
     return file;
   }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
+  public synchronized String getContent() throws IOException {
+    int data = inputStream.read();
+    while (data > 0) {
       output += (char) data;
     }
     return output;
   }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
+  public synchronized String getContentWithoutUnicode() throws IOException {
+    int data = inputStream.read();
+    while (data > 0) {
       if (data < 0x80) {
         output += (char) data;
       }
     }
     return output;
   }
-  public void saveContent(String content) throws IOException {
+  public synchronized void saveContent(String content) throws IOException {
     FileOutputStream o = new FileOutputStream(file);
     for (int i = 0; i < content.length(); i += 1) {
       o.write(content.charAt(i));
