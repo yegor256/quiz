86e4f8d8abf8ad1e8c0673599b3c5442d78b6d1c
Altaf <altaf933@gmail.com>
2015-09-22 22:24:39 +0530

    1)Release recourse associated with raw byte stream. 2)Add comment in method 3)Make variable name significant. 4) Add constructor to access file.

--- Parser.java
+++ Parser.java
@@ -6,37 +6,103 @@ import java.io.IOException;
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+	private File file;
+
+	 
+
+	/*
+	 * set file name
+	 * 
+	 * @Param file set file object.
+	 */
+	public Parser(File file) {
+		this.file = file;
+	}
+
+	/**
+	 * Read content of file in byte stream than convert to string
+	 * 
+	 * */
+	public String getContent() {
+		FileInputStream fis = null;
+		String output = "";
+		try {
+			fis = new FileInputStream(file);
+			int data;
+			while ((data = fis.read()) > 0) {
+				output += (char) data;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fis != null)
+					fis.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+
+		return output;
+	}
+	/**
+	 * Read content of file in byte stream than convert to string
+	 * 
+	 * */
+	public String getContentWithoutUnicode() {
+		FileInputStream fis = null;
+		String output = "";
+		try {
+			fis = new FileInputStream(file);
+
+			int data;
+			while ((data = fis.read()) > 0) {
+				if (data < 0x80) {
+					output += (char) data;
+				}
+			}
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fis != null)
+					fis.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+
+		return output;
+	}
+
+	/**
+	 * Write a byte of data into file If not any file than create it
+	 * 
+	 * */
+	/**
+	 * Write a byte of data into file If not any file than create it
+	 * 
+	 * */
+	public synchronized void saveContent(String content) {
+		FileOutputStream fout = null;
+		try {
+			fout = new FileOutputStream(file);
+			for (int i = 0; i < content.length(); i += 1) {
+				fout.write(content.charAt(i));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fout != null)
+					fout.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+
+	}
 }
