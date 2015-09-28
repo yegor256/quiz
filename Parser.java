827771ff31da22ca23b015d3efa328e363727bdb
Ramchander <rams2588@gmail.com>
2015-09-29 00:43:35 +0530

    Removed redundant code and added exception handling

--- Parser.java
+++ Parser.java
@@ -6,37 +6,93 @@ import java.io.IOException;
  * This class is thread safe.
  */
 public class Parser {
+
+  /** variable to hold the file object */
   private File file;
+  
+  /**
+   * Method to set the file object
+   * @param f
+   */
   public synchronized void setFile(File f) {
     file = f;
   }
+  
+  /**
+   * Method to get the file object
+   * @return
+   */
   public synchronized File getFile() {
     return file;
   }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
+  
+  /**
+   * Method to get the content as string
+   * @return string
+   * @throws IOException
+   */
+  public String getContent() throws IOException {	
+    return getContent(true);
   }
+  
+  /**
+   * Method to get the content withoug UniCode
+   * @return string
+   * @throws IOException
+   */
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
+    return getContent(false);
   }
+  
+  /**
+   * Method get the content based on the unicode flag
+   * @param uniCodeFlag
+   * @return
+   * @throws IOException
+   */
+  private String getContent(boolean uniCodeFlag) throws IOException {
+		FileInputStream fileInputStream = null;
+		String output = "";
+	    try {
+		  fileInputStream = new FileInputStream(file);      
+	      int data;
+	      while ((data = fileInputStream.read()) > 0) {
+	    	if (uniCodeFlag) {
+	    	  output += (char) data;
+	    	} else if (data < 0x80) {
+	          output += (char) data;
+	        }
+	      } 
+	    } catch (IOException e) {
+			//TODO log the meesage
+			throw e;
+		} finally {
+			if (fileInputStream != null) {
+				fileInputStream.close();
+			}
+		}
+		return output;
+	}
+  
+  /**
+   * Method to save the content to file
+   * @param content
+   * @throws IOException
+   */
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
+	FileOutputStream o = null;
+	try {
+	    o = new FileOutputStream(file);
+	    for (int i = 0; i < content.length(); i += 1) {
+	      o.write(content.charAt(i));
+	    }
+	} catch (IOException e) {
+		//TODO log the meesage
+		throw e;
+	} finally {
+		if (o != null) {
+			o.close();
+		}
+	}
   }
 }
