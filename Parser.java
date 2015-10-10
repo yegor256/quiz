222ba14e90342440f8a833f53d1f91e65dca9de0
Mike Shadid <mike.shadid@gmail.com>
2015-10-10 18:06:49 -0400

    Refactored Parser to deal with concurrency issues.

--- Parser.java
+++ Parser.java
@@ -5,38 +5,55 @@ import java.io.IOException;
 /**
  * This class is thread safe.
  */
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+	private File file;
+	private String content = null;
+	private String contentWithoutUnicode = null;
+
+	public Parser(File f) {
+		file = f;
+	}
+
+	public synchronized String getContent() throws IOException {
+		if (content == null) {
+			FileInputStream i = new FileInputStream(file);
+			content = "";
+			int data;
+			while ((data = i.read()) > 0) {
+				content += (char) data;
+			}
+			i.close();
+		}
+		return content;
+	}
+
+	public synchronized String getContentWithoutUnicode() throws IOException {
+		if (contentWithoutUnicode == null) {
+			FileInputStream i = new FileInputStream(file);
+			contentWithoutUnicode = "";
+			int data;
+			while ((data = i.read()) > 0) {
+				if (data < 0x80) {
+					contentWithoutUnicode += (char) data;
+				}
+			}
+			i.close();
+		}
+		return contentWithoutUnicode;
+	}
+
+	public synchronized void saveContent(String content) throws IOException {
+		FileOutputStream o = new FileOutputStream(file);
+		for (int i = 0; i < content.length(); i += 1) {
+			o.write(content.charAt(i));
+		}
+		o.close();
+		content = null;
+		contentWithoutUnicode = null;
+	}
 }
