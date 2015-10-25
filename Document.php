38643022b1fc7d9552ca7a3997249ec759266ad9
Armin Braun <armin.b@icanlocalize.com>
2015-10-25 23:33:32 +0100

    removed magic numbers, duplication, public property access and missing caching for \Document::getAllDocuments `s return in \User and \Document

--- Document.php
+++ Document.php
@@ -1,49 +1,65 @@
 <?php
 class Document {
 
-    public $user;
+    const CONTENT_COL = 6;
+    const TITLE_COL = 3;
 
-    public $name;
+    private $user;
 
-    public function init($name, User $user) {
+    private $name;
+
+    public function __construct($name, User $user) {
         assert(strlen($name) > 5);
         $this->user = $user;
         $this->name = $name;
     }
 
+    public function get_user(){
+
+        return $this->user;
+    }
+
+    public function get_name(){
+
+        $this->name;
+    }
+
     public function getTitle() {
-        $db = Database::getInstance();
-        $row = $db->query('SELECT * FROM document WHERE name = "' . $this->name . '" LIMIT 1');
-        return $row[3]; // third column in a row
+
+        return $this->get_attribute(self::TITLE_COL); // third column in a row
     }
 
     public function getContent() {
-        $db = Database::getInstance();
-        $row = $db->query('SELECT * FROM document WHERE name = "' . $this->name . '" LIMIT 1');
-        return $row[6]; // sixth column in a row
+
+        return $this->get_attribute(self::CONTENT_COL);
     }
 
     public static function getAllDocuments() {
         // to be implemented later
     }
 
+    private function get_attribute($column_number){
+        $db = Database::getInstance();
+        $row = $db->query('SELECT * FROM document WHERE name = "' . $this->name . '" LIMIT 1');
+        return $row[$column_number]; // third column in a row
+    }
 }
 
 class User {
 
     public function makeNewDocument($name) {
-        $doc = new Document();
-        $doc->init($name, $this);
+        $doc = new Document($name, $this);
+
         return $doc;
     }
 
     public function getMyDocuments() {
         $list = array();
-        foreach (Document::getAllDocuments() as $doc) {
-            if ($doc->user == $this)
+        $all_docs = Document::getAllDocuments();
+        foreach ($all_docs as $doc) {
+            if ($doc->get_user() == $this)
                 $list[] = $doc;
         }
         return $list;
     }
-
 }
