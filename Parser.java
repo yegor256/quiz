6b004f0e1972348e09e2dce67670f2ea4dbc436e
Anuradha G <theanuradha@gmail.com>
2015-11-03 15:11:06 +0530

    fix thread safe issues

--- Parser.java
+++ Parser.java
@@ -6,7 +6,10 @@ import java.io.IOException;
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
+  private volatile File file;
+  
+  private final Object LOCK = new Object();
+  
   public synchronized void setFile(File f) {
     file = f;
   }
@@ -14,29 +17,71 @@ public class Parser {
     return file;
   }
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+      
+   FileInputStream i;
+    synchronized (LOCK)
+    {
+        i = new FileInputStream(file);  
     }
-    return output;
+   
+    try
+    {
+    
+        StringBuilder output = new StringBuilder();
+        int data;
+        while ((data = i.read()) > 0) {
+          output.append((char) data);
+        }
+        return output.toString();
+        
+    
+    }
+    finally
+    {
+        i.close();
+    }
   }
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
+     
+      FileInputStream i;
+      synchronized (LOCK)
+      {
+          i = new FileInputStream(file);  
       }
-    }
-    return output;
+     
+      try
+      {
+      
+          StringBuilder output = new StringBuilder();
+          int data;
+          while ((data = i.read()) > 0) {
+              if (data < 0x80) 
+                  output.append((char) data);
+          }
+          return output.toString();
+          
+      
+      }
+      finally
+      {
+          i.close();
+      }
   }
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+      
+    synchronized (LOCK)
+    {
+        FileOutputStream o = new FileOutputStream(file);
+        try
+        {
+            for (int i = 0; i < content.length(); i += 1) {
+              o.write(content.charAt(i));
+            }
+        }
+        finally
+        {
+            o.close();
+        }
     }
   }
 }
