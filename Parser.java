d6f2dfaa3fdbe7472b101dd382fb99b6b7e1476e
sghosh <sghosh.jee@gmail.com>
2015-11-04 12:12:17 +0530

    refactored / commented

--- Parser.java
+++ Parser.java
@@ -6,34 +6,48 @@ import java.io.IOException;
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+
+  // should be at least one empty line between each methods
+
+  // it might be a good idea to make indent 4 spaces
+
+  // removed file variable, also related getter / setter, as it is should not the part of Parser
+
+  // file to be passed as parameter
+  public String getContent(File file) throws IOException {
     String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
+    // missing try with resources
+    // use of single charcter variable should be restricted
+    try (FileInputStream i = new FileInputStream(file)){
+      int data;
+      // check should be for >= 0 for eof
+      while ((data = i.read()) >= 0) {
+        // use StringBuilder here
         output += (char) data;
       }
     }
     return output;
   }
-  public void saveContent(String content) throws IOException {
+
+  // file to be passed as parameter
+  public String getContentWithoutUnicode(File file) throws IOException {
+    // missing try with resources - as shown in getContent() above
+    FileInputStream i = new FileInputStream(file);
+    String output = "";
+    int data;
+    // check should be for >= 0 for eof
+    while ((data = i.read()) >= 0) {
+      if (data < 0x80) {
+        // use StringBuilder here
+        output += (char) data;
+      }
+    }
+    return output;
+  }
+
+  // file to be passed as parameter
+  public void saveContent(File file, String content) throws IOException {
+    // missing try with resources - as shown in getContent() above
     FileOutputStream o = new FileOutputStream(file);
     for (int i = 0; i < content.length(); i += 1) {
       o.write(content.charAt(i));
