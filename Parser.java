569414ef8cd24280ac7f8d27dff19b9055ef3f7e
Bassem NAFFETI <naffeti.bassem@gmail.com>
2015-11-04 09:04:57 +0100

    Refactoring Parser.java
    //Changes :
    - Repalce getContent and getContentWithoutUnicode with one generic method getContent(boolean WithoutUnicode)
    - FileInputStream and FileOutPutStream must be closed after reading and writting.
    - Use StringBuilder instead of String for better performance
    - Add synchronized to the methods.
    - Surround methods by try/catch better than throws it, for better management of errors and logs
    - add javadoc and comments to the class
    
    //TODO
    - we must write some unit test methods for the Parser class

--- Parser.java
+++ Parser.java
@@ -1,42 +1,78 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+
+	/** The file attribute */
+	private File file;
+
+
+	/**
+	 * getContent method.
+	 * @return String content
+	 */
+	public synchronized String getContent(boolean WithoutUnicode) {
+		StringBuilder output = new StringBuilder("");
+		try {
+			FileInputStream fileInputStream = new FileInputStream(file);
+			int data;
+			while ((data = fileInputStream.read()) > 0) {
+				// if WithoutUnicode
+				if (WithoutUnicode && (data < 0x80)) {
+					output.append((char) data);
+				} else {
+					output.append((char) data);
+				}
+			}
+			// Close fileInputStream
+			fileInputStream.close();
+		// catch exceptions
+		} catch (FileNotFoundException fnfException) {
+			System.err.println("File not fount : " + fnfException.getMessage());
+		} catch (IOException ioException) {
+			System.err.println("caught exception : " + ioException.getMessage());
+		}
+		return output.toString();
+	}
+
+	/**
+	 * saveContent method.
+	 */
+	public synchronized void saveContent(String content){
+		try {
+			FileOutputStream fileOutputStream = new FileOutputStream(file);
+			for (int i = 0; i < content.length(); i += 1) {
+				fileOutputStream.write(content.charAt(i));
+			}
+			// Close fileOutputStream
+			fileOutputStream.close();
+			// catch exceptions
+		} catch (FileNotFoundException fnfException) {
+			System.err.println("File not fount : " + fnfException.getMessage());
+		} catch (IOException ioException) {
+			System.err.println("caught exception : " + ioException.getMessage());
+		}
+	}
+	
+	/**
+	 * Setter of file attribute.
+	 * @param f File
+	 */
+	public synchronized void setFile(File f) {
+		this.file = f;
+	}
+
+	/**
+	 * Getter of file attribute.
+	 * @return file
+	 */
+	public synchronized File getFile() {
+		return this.file;
+	}
 }
