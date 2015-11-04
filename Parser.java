7d3e5384cf7a8248edcbffa9fbd9bae440da06dd
Cloves Almeida <cjalmeida@gmail.com>
2015-11-04 16:38:10 -0200

    Refactored Parser.java

--- Parser.java
+++ Parser.java
@@ -1,42 +1,51 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
+
+  private final File file;
+
+  public Parser(File file) {
+  	this.file = file;
   }
-  public synchronized File getFile() {
+  
+  public File getFile() {
     return file;
   }
+
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
+    return getContent(true);
   }
+
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
+    return getContent(false);
   }
+
+  protected String getContent(boolean withUnicode) throws IOException {
+    BufferedInputStream i = new BufferedInputStream(new FileInputStream(file));
+    try {
+      StringBuilder output = new StringBuilder();
+      int data;
+      while ((data = i.read()) > 0) {
+        if (withUnicode || data < 0x80) {
+          output.append((char) data);
+        }
+      }
+      return output.toString();
+    } finally {
+      i.close();
+    }
+  }
+
+
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+    BufferedOutputStream o = new BufferedOutputStream(new FileOutputStream(file));
+    try {
+      o.write(content.getBytes());
+    } finally {
+      o.close();
     }
   }
 }
