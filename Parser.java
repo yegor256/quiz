c3024f2cfed29c4ddddbae510165b6358aafda5f
Maksimenko Vladimir <xupypr@xupypr.com>
2015-11-07 21:43:16 +0300

    1. This class is useless without filled `file` field. So, I create a constructor with `file` parameter. Remove getter and setter for `file` (if you want to parse another file - just create new Parser)
    2. This class is used to read file to string, and write string to file. So, I
    limit file size up to 100Mb
    
    3. I use Files.readAllBytes inside getContent() and Files.write inside
    saveContent() to up a performance and make code easy to understand (Old
    method was ineffective: 1) read by one byte is slow 2) string concatenation
    creates a lot of garbage and have poor performance)
    
    4. I made field `file` final and synchronize getContent/saveContent by this
    field to warranty thread safety.
    
    5. I made some junit tests to check that class works properly

--- Parser.java
+++ Parser.java
@@ -1,42 +1,64 @@
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 /**
- * @deprecated use Files.readAllBytes and Files.write instead (JRE 1.7 and above required) 
+ * <p>
+ * This class is used to read file content to string (optionally without unicode
+ * characters) and replace file content by string value. This class is thread safe.
+ * </p>
+ * <b>NOTE!</b> File size is limited up to 100Mb
  */
-public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+public class Parser
+{
+	private final File file;
+
+	public Parser(File file)
+	{
+		if (!file.exists())
+			throw new IllegalArgumentException("Parameter `file` must point to already created file");
+		if (file.isDirectory())
+			throw new IllegalArgumentException("Parameter `file` must point to file, not to directory");
+		if (file.length() / 1048576 > 100)
+			throw new IllegalArgumentException("Parameter `file` must point to file that not exceeds 100Mb size");
+
+		this.file = file;
+	}
+
+	public String getContent() throws IOException
+	{
+		synchronized (file)
+		{
+			byte[] encoded = Files.readAllBytes(Paths.get(file.toURI()));
+			return new String(encoded);
+		}
+	}
+
+	public String getContentWithoutUnicode() throws IOException
+	{
+		synchronized (file)
+		{
+			BufferedInputStream f = new BufferedInputStream(new FileInputStream(file));
+			StringBuilder output = new StringBuilder();
+			int data = 0;
+			while ((data = f.read()) != -1)
+				if (data < 0x80)
+					output.append((char) data);
+			f.close();
+			return output.toString();
+		}
+	}
+
+	public void saveContent(String content) throws IOException
+	{
+		synchronized (file)
+		{
+			Files.write(Paths.get(file.toURI()), content.getBytes(), StandardOpenOption.CREATE,
+					StandardOpenOption.TRUNCATE_EXISTING);
+		}
+	}
 }
