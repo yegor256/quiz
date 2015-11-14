3a801252f8cc027ee60e5faf2e15e3fbb1282250
Fernando Farias <yiowns@gmail.com>
2015-11-13 23:19:27 -0300

    Hurried 15min improvements.

--- Parser.java
+++ Parser.java
@@ -1,42 +1,47 @@
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+    private File file;
+
+    public synchronized void setFile(File f) {
+        file = f;
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    public synchronized File getFile() {
+        return file;
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    public String getContent() throws IOException {
+        return new String(Files.readAllBytes(file.toPath()));
     }
-  }
+
+    /**
+     * Get content filtered without unicode characters above 0x80.
+     *
+     * @return Filtered content.
+     * @throws IOException Under error.
+     */
+    public String getContentWithoutUnicode() throws IOException {
+        try (FileInputStream in = new FileInputStream(file)) {
+            return Stream.of(in.read())
+                    .filter(data -> data < 0x80)
+                    .collect(Collector.of(
+                            StringBuilder::new,
+                            StringBuilder::appendCodePoint,
+                            StringBuilder::append,
+                            StringBuilder::toString));
+        }
+    }
+
+    public void saveContent(String content) throws IOException {
+        Files.write(file.toPath(), content.getBytes());
+    }
 }
