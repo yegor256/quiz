06ecabed382e09d2cfd0421d2b97bbca6793eb30
imtiazislam <imtiazislam@users.noreply.github.com>
2015-11-14 14:15:56 +0600

    Update Parser.java
    Refactored code.  Used StringBuilder, BufferedInputStream and BufferedOutputStream to improve performance.  Closed the Streams to avoid leaks

--- Parser.java
+++ Parser.java
@@ -1,42 +1,49 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
   private File file;
+  
   public synchronized void setFile(File f) {
     file = f;
   }
+  
   public synchronized File getFile() {
     return file;
   }
+  
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
+	  return prepareContent(false);
   }
+  
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
+    return prepareContent(true);
   }
+  
+  private String prepareContent(boolean withoutUnicode) throws IOException {
+	  BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(file));
+	  StringBuilder output = new StringBuilder();
+	  int data;
+	  while ((data = bufferedInputStream.read()) > 0) {
+		  if (withoutUnicode && !(data < 0x80)) continue;
+	    output.append((char) data);
+	  }
+	  bufferedInputStream.close();
+	  return output.toString();
+  }
+  
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
+	  BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(file));
     for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+    	bufferedOutputStream.write(content.charAt(i));
     }
+	  bufferedOutputStream.close();
   }
 }
