f5d998dc8c677551316b79544422058d7003017d
Ivan Junckes Filho <ivanjunckes@gmail.com>
2015-11-25 09:10:04 -0200

    Refactoring quiz class

--- Parser.java
+++ Parser.java
@@ -2,41 +2,74 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
+ * 
+ * Change class name to ContentService Remove "Content" from the methods
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+
+	private File file;
+
+	public synchronized void setFile(File f) {
+		this.file = f;
+	}
+
+	public synchronized File getFile() {
+		return this.file;
+	}
+
+	public synchronized String getContent() throws IOException {
+		FileInputStream inputStream = null;
+		String content = "";
+
+		try {
+			inputStream = new FileInputStream(file);
+			int data;
+			while ((data = inputStream.read()) > 0) {
+				content += (char) data;
+			}
+
+		} finally {
+			if (inputStream != null)
+				inputStream.close();
+		}
+		return content;
+	}
+
+	public synchronized String getContentWithoutUnicode() throws IOException {
+		FileInputStream inputStream = null;
+		String content = "";
+
+		try {
+			inputStream = new FileInputStream(file);
+			int data;
+			while ((data = inputStream.read()) > 0) {
+				if (data < 0x80) {
+					content += (char) data;
+				}
+			}
+		} finally {
+			if (inputStream != null)
+				inputStream.close();
+		}
+		return content;
+	}
+
+	public synchronized void saveContent(String content) throws IOException {
+		FileOutputStream output = null;
+		
+		try {
+			output = new FileOutputStream(file);
+			for (char c : content.toCharArray()) {
+				output.write(c);
+			}
+		} finally {
+			if (output != null) {
+				output.flush();
+				output.close();
+			}
+		}
+	}
 }
