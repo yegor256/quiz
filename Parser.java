57e8beb0c7d54b3fb5381baa4283f68714e1f011
Gaurav Shah <shahg@ttg.global>
2015-12-09 16:31:04 +0530

    Refactored

--- Parser.java
+++ Parser.java
@@ -2,6 +2,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 /**
  * This class is thread safe.
  */
@@ -13,30 +16,48 @@ public class Parser {
   public synchronized File getFile() {
     return file;
   }
-  public String getContent() throws IOException {
+  
+    // read and write operations should be synchronized in multithreading context.
+  public synchronized String getContent() throws IOException {
     FileInputStream i = new FileInputStream(file);
-    String output = "";
+	// use StringBuilder instead of string 
+    StringBuilder output = new StringBuilder();
     int data;
     while ((data = i.read()) > 0) {
-      output += (char) data;
+      output.append((char)data);
     }
-    return output;
+	// close operation necessary to remove memory leak.
+    i.close();
+    return output.toString();
   }
-  public String getContentWithoutUnicode() throws IOException {
+  
+    // read and write operations should be synchronized in multithreading context.
+  public synchronized String getContentWithoutUnicode() throws IOException {
     FileInputStream i = new FileInputStream(file);
-    String output = "";
+	
+	// to remove unicode characters we use Reader class as it uses default character encoding.
+    Reader r = new InputStreamReader(i);
+	
+	// use StringBuilder instead of string 
+    StringBuilder output = new StringBuilder();
     int data;
     while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+        output.append((char)data);
+      
     }
-    return output;
+	// close operation necessary to remove memory leak.
+    r.close();
+    return output.toString();
   }
+  
+  // read and write operations should be synchronized in multithreading context.
   public void saveContent(String content) throws IOException {
     FileOutputStream o = new FileOutputStream(file);
     for (int i = 0; i < content.length(); i += 1) {
       o.write(content.charAt(i));
     }
+	// close operation necessary to remove memory leak.
+    o.close();
   }
+
 }
