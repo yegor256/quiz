429421598521da048cca1d518e972c747915fb62
Andrey Eliseev <andrey.eliseev@db.com>
2015-12-14 23:40:58 +0300

    - volatile instead of synchronized - buffered read - finally/close on file streams - only one optimized method for file read - optimized method for file write

--- Parser.java
+++ Parser.java
@@ -1,42 +1,67 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.regex.Pattern;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+    private volatile File file;
+    private static final int DEFAULT_BUFFER_SIZE = 4096;
+    private static final Pattern REGEX_NON_ASCII = Pattern.compile("[^\\x00-\\x7F]");
+
+    public void setFile(File f) {
+        file = f;
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    public File getFile() {
+        return file;
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    /**
+     * @return unfiltered content of the file
+     * @throws IOException
+     */
+    public String getContent() throws IOException {
+        FileInputStream input = null;
+        try {
+            input = new FileInputStream(file);
+            InputStreamReader reader = new InputStreamReader(input);
+            StringWriter writer = new StringWriter();
+            char[] buffer = new char[DEFAULT_BUFFER_SIZE];
+            while (reader.read(buffer) > 0) {
+                writer.write(buffer);
+            }
+            return writer.toString();
+        } finally {
+            if (input != null) {
+                input.close();
+            }
+        }
     }
-  }
+
+    /**
+     * @return filtered content of the file - only ASCII symbols
+     * @throws IOException
+     */
+    public String getContentWithoutUnicode() throws IOException {
+        return REGEX_NON_ASCII.matcher(getContent()).replaceAll("");
+    }
+
+    /**
+     * Saves data to the file
+     *
+     * @param content String data to save into file
+     * @throws IOException
+     */
+    public void saveContent(String content) throws IOException {
+        FileOutputStream o = null;
+        try {
+            o = new FileOutputStream(file);
+            o.write(content.getBytes());
+        } finally {
+            if (o != null) {
+                o.close();
+            }
+        }
+    }
 }
