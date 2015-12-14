dd67f52c947b17bdee7c39846ec5f29c995db9ec
Vadim Zoludev <vadim.zoludev@aktkc.lt>
2015-12-14 21:38:42 +0200

    Fixes

--- Parser.java
+++ Parser.java
@@ -1,36 +1,36 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
+    private File file;
 
-  public Parser(File file) {
-    this.file = file;
-  }
+    public Parser(File file) {
+        this.file = file;
+    }
 
-  public String getContent() throws IOException {
-    return getContentWithoutUnicode(false)
-  }
-  public String getContentWithoutUnicode(boolean skipUnicode) throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (skipUnicode && data < 0x80) {
-        continue;
-      }
-      output += (char) data;
+    public String getContent() throws IOException {
+        return getContentWithoutUnicode(false)
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    public String getContentWithoutUnicode(boolean skipUnicode) throws IOException {
+        FileInputStream i = new FileInputStream(file);
+        String output = "";
+        int data;
+        while ((data = i.read()) > skipUnicode ? 0x80 : 0) {
+            output += (char) data;
+        }
+        return output;
     }
-  }
+
+    /**
+     * This method is not thread safe. If 2 threads will invoke this method - exception will be rised.
+     */
+    public void saveContent(String content) throws IOException {
+        FileOutputStream o = new FileOutputStream(file);
+        for (int i = 0; i < content.length(); i += 1) {
+            o.write(content.charAt(i));
+        }
+    }
 }
