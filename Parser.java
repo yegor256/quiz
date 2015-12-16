bfa58304c21d05b4808cb96d3c31a247b3a1eb2a
Yevgeny Terov <terov@autosat.com>
2015-12-16 07:15:14 +0300

    Parser fearless refactoring

--- Parser.java
+++ Parser.java
@@ -2,41 +2,33 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
+  private static final Charset CHARSET = StandardCharsets.UTF_8;
+  private static final Pattern REGEX_NON_ASCII = Pattern.compile("[^\\x00-\\x7F]");
+  private final File file;
+
+  public Parser(File file) {
+    this.file = file;
   }
-  public synchronized File getFile() {
-    return file;
-  }
+
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
+    return new String(Files.readAllBytes(file.toPath()), CHARSET);
   }
+
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
+    return REGEX_NON_ASCII.matcher(getContent()).replaceAll("");
   }
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
+    Files.write(file.toPath(), content.getBytes(CHARSET));
   }
 }
