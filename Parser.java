8275b49f140ea299b4584a4c1e98bcb3ff90c7c2
Seifeddine Bouallegue <saief.bouallegue@gmail.com>
2015-12-24 11:50:38 +0300

    Refactor getContent() and close File Streams
    Refactor getContent(): add parameter for Unicode mode.
    A good practice is to close File Streams to release system resources.

--- Parser.java
+++ Parser.java
@@ -2,41 +2,53 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
   private File file;
+
   public synchronized void setFile(File f) {
     file = f;
   }
+
   public synchronized File getFile() {
     return file;
   }
-  public String getContent() throws IOException {
+
+  public String getContent(boolean withUnicode) throws IOException {
     FileInputStream i = new FileInputStream(file);
     String output = "";
     int data;
+
     while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
+      char toAdd = '';
+
+      if (!withUnicode) {
+        if (data < 0x80) {
+          toAdd = (char) data;
+        }
+      } else {
+        toAdd = (char) data;
       }
+
+      output += toAdd;
     }
+
+    i.close();
     return output;
   }
+
+  public String getContentWithoutUnicode() throws IOException {
+    return getContent(false);
+  }
+
   public void saveContent(String content) throws IOException {
     FileOutputStream o = new FileOutputStream(file);
     for (int i = 0; i < content.length(); i += 1) {
       o.write(content.charAt(i));
     }
+    o.close();
   }
 }
