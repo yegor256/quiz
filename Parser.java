ce7f29dd792f2002901ebcf14ded75ad99211a6f
Matt Gumbel <MGumbel@ExtensionHealthcare.com>
2016-01-06 17:40:28 -0500

    Refactored Parser
    Refactored Parser to reduce duplicate code an assure thread safety

--- Parser.java
+++ Parser.java
@@ -1,42 +1,103 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+
 /**
- * This class is thread safe.
+ * This class is thread safe that is used to read and write contents to and from
+ * a file.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+    /**
+     * The file that will be parsed
+     */
+    private final File file;
+
+    /**
+     * Constructs a Parser
+     *
+     * @param file
+     *            the file that will be parsed
+     */
+    public Parser(File file) {
+        this.file = file;
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    /**
+     * Retrieves the contents of the file
+     *
+     * @return String with the contents of the file
+     * @throws IOException
+     *             if there is a problem reading the file
+     */
+    public synchronized String getContent() throws IOException {
+        return getContent(false);
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    /**
+     * Retrieves the contents of the file
+     *
+     * @param isBasicUnicode
+     *            true to indicate the contents should return only standard
+     *            unicode values
+     * @return the contents of the file as a String
+     * @throws IOException
+     *             if there is a problem reading the file
+     */
+    private String getContent(boolean isBasicUnicode) throws IOException {
+        if (file == null) {
+            return "";
+        }
+
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            String output = "";
+
+            int data;
+            while ((data = in.read()) > 0) {
+                if (!isBasicUnicode || isBasicUnicode && data < 0x80) {
+                    output += (char) data;
+                }
+            }
+            return output;
+        }
     }
-  }
+
+    /**
+     * Retrieves the contents of the file without extended unicode characters
+     *
+     * @return the contents of the file as a String
+     * @throws IOException
+     *             if there is a problem reading the file
+     */
+    public synchronized String getContentWithoutUnicode() throws IOException {
+        return getContent(true);
+    }
+
+    /**
+     * Returns the file being parsed
+     *
+     * @return the File being parsed
+     */
+    public File getFile() {
+        return file;
+    }
+
+    /**
+     * Saves the supplied content to the file
+     *
+     * @param content
+     *            the content to write to the file
+     * @throws IOException
+     *             if there is a problem writing to the file
+     */
+    public synchronized void saveContent(String content) throws IOException {
+        if (content != null && file != null) {
+            try (BufferedWriter out = new BufferedWriter(new FileWriter(file))) {
+                out.write(content);
+            }
+        }
+    }
 }
