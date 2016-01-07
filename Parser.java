518aa1bbe2d809f8ea05f4f68840be4b73a1b96d
Patroklos Papapetrou <ppapapetrou76@gmail.com>
2016-01-07 23:29:15 +0200

    Fix duplication by applying a single strategy pattern

--- Parser.java
+++ Parser.java
@@ -2,6 +2,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 /**
  * This class is thread safe.
  */
@@ -13,30 +21,39 @@ public class Parser {
   public synchronized File getFile() {
     return file;
   }
-  public String getContent() throws IOException {
+  public String getContent(DataAdderStrategy adder) throws IOException {
     FileInputStream i = new FileInputStream(file);
     String output = "";
     int data;
     while ((data = i.read()) > 0) {
-      output += (char) data;
+      adder.addData(output,data);
     }
     return output;
   }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
+
   public void saveContent(String content) throws IOException {
     FileOutputStream o = new FileOutputStream(file);
     for (int i = 0; i < content.length(); i += 1) {
       o.write(content.charAt(i));
     }
   }
+
+
+  private interface DataAdderStrategy {
+    public void addData(String output, int data);
+  }
+
+  public static class DefaultDataAdder implements DataAdderStrategy{
+    public void addData(String output, int data){
+      output += (char) data;
+    }
+  }
+  public static class EncodingDataAdder implements DataAdderStrategy{
+    public void addData(String output, int data){
+      if (data < 0x80) {
+        output += (char) data;
+      }
+    }
+  }
+}
 }
