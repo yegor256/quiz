92f5e49d75f9ee7790bfc0966386ba43d2d7d3d2
lexx <lexx@i-free.com>
2016-01-14 02:24:42 +0300

    !

--- Parser.java
+++ Parser.java
@@ -2,41 +2,56 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
+ * @author Alexey Krylov
+ * @since 14.01.2016
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+    /**
+     * ASCII character set ending.
+     */
+    private static final int C_ASCII_END = 0x80;
+    private File file;
+
+    public synchronized void setFile(File f) {
+        file = f;
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    public synchronized File getFile() {
+        return file;
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    public synchronized String getContent() throws IOException {
+        try (FileInputStream i = new FileInputStream(file)) {
+            StringBuilder sb = new StringBuilder();
+            int data;
+            while ((data = i.read()) > 0) {
+                sb.append((char) data);
+            }
+            return sb.toString();
+        }
     }
-  }
+
+    public synchronized String getContentWithoutUnicode() throws IOException {
+        try (FileInputStream i = new FileInputStream(file)) {
+            StringBuilder sb = new StringBuilder();
+            int data;
+            while ((data = i.read()) > 0) {
+                if (data < C_ASCII_END) {
+                    sb.append((char) data);
+                }
+            }
+            return sb.toString();
+        }
+    }
+
+    public synchronized void saveContent(String content) throws IOException {
+        try (FileOutputStream o = new FileOutputStream(file)) {
+            for (int i = 0; i < content.length(); i += 1) {
+                o.write(content.charAt(i));
+            }
+        }
+    }
 }
