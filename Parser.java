a2de1b28b4956fe713e5b6a2b32d1286fe4c32d8
Thang Hoang <thang.hoang@vndirect.com.vn>
2016-01-23 22:43:49 +0700

    refactored after 30 minute

--- Parser.java
+++ Parser.java
@@ -1,42 +1,34 @@
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+    private File file;
+    public void setFile(File f) {
+        file = f;
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+    public synchronized String getContent() throws IOException {
+        return new String(Files.readAllBytes(file.toPath()));
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+    public String getContentWithoutUnicode() throws IOException {
+        String content = getContent();
+        StringBuffer output = new StringBuffer();
+        for (int i = 0; i < content.length(); i++) {
+            char character = content.charAt(i);
+            if (character < 0x80) {
+                output.append(character);
+            }
+        }
+        return output.toString();
     }
-  }
+    public synchronized void saveContent(String content) throws IOException {
+        try (FileOutputStream output = new FileOutputStream(file)) {
+            output.write(content.getBytes());
+            output.close();
+        }
+    }
 }
