ed21d550569dabd37e4c1ff65436fa70c63a6ac3
Mani Rai <rai.mani.37@gmail.com>
2016-01-31 22:01:03 +0545

    Redesigned Refactor Recommendation.

--- Parser.java
+++ Parser.java
@@ -11,55 +11,77 @@ import java.io.OutputStreamWriter;
  * Changed class name because this class object alone cannot handle parsing all
  * types of files. If made to parse file of all types, code will be too verbose
  * (really bad code and program). So lets assume its a text file parser only.
- * Moreover we can use Parser class as an abstract class inherited by file type
- * specific classes like TextFileParser with their own read/write implementation
- * specific to file types. Replicates real world to a program.
- * 
- * This class was neither thread safe before nor now. Need to come up with file
- * read/write plan like read write locking, single thread for file writing which
- * is out of the scope of refactoring.
  */
 public class TextFileParser {
 
 	private File file;
 
-	public synchronized void setFile(File f) {
-		file = f;
+	/**
+	 * Assuming file parsers object would not be singleton. Parameterized
+	 * constructor to make file parsers object immutable. This will make the
+	 * file parsers thread safe not allowing to change the file by other thread
+	 * and also prevents NPE.
+	 */
+	public TextFileParser(File file) {
+		this.file = file;
 	}
 
-	public synchronized File getFile() {
-		return file;
+	public File getFile() {
+		return this.file;
 	}
 
 	/**
 	 * Algorithm refactored for more expressive codes.
 	 */
 	public String getContent() throws IOException {
-		
-		// Reading file as bytes.
-		FileInputStream inputStream = new FileInputStream(file);
 
-		// Bridging for easy conversion from byte to character streams with
-		// required encoding.
-		InputStreamReader inputStreamReader = new InputStreamReader(
-				inputStream, "UTF-8");
-
-		// Chaining character stream to buffered reader for the efficient
-		// reading of characters, arrays, and lines.
-		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-		// Using string builder instead of string concatenation for optimized
-		// performance.
+		// Local variable must be initialized.
+		FileInputStream inputStream = null;
+		InputStreamReader inputStreamReader = null;
+		BufferedReader bufferedReader = null;
 		StringBuilder output = new StringBuilder();
-		String line;
 
-		while ((line = bufferedReader.readLine()) != null) {
-			output.append(line);
+		try {
+
+			// Reading file as bytes.
+			inputStream = new FileInputStream(this.file);
+
+			// Bridging for easy conversion from byte to character streams with
+			// required encoding.
+			inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+
+			// Chaining character stream to buffered reader for the efficient
+			// reading of characters, arrays, and lines.
+			bufferedReader = new BufferedReader(inputStreamReader);
+
+			// Using string builder instead of string concatenation for
+			// optimized
+			// performance.
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				output.append(line);
+			}
+		} catch (IOException e) {
+
+			// Logging needed.
+			// It would be good to handle exception in business layer.
+			// So that appropriate response could be built based on the
+			// exceptions caught.
+			throw e;
+		} finally {
+
+			if (bufferedReader != null) {
+				bufferedReader.close();
+			}
+
+			if (inputStreamReader != null) {
+				inputStreamReader.close();
+			}
+
+			if (inputStream != null) {
+				inputStream.close();
+			}
 		}
-
-		bufferedReader.close();
-		inputStreamReader.close();
-		inputStream.close();
 
 		return output.toString();
 	}
@@ -68,27 +90,47 @@ public class TextFileParser {
 	 * Algorithm refactored for more expressive codes.
 	 */
 	public String getContentWithoutUnicode() throws IOException {
-		
-		// Reading file as bytes.
-		FileInputStream inputStream = new FileInputStream(file);
 
-		// Bridging for easy conversion from byte to character streams with
-		// required encoding.
-		InputStreamReader inputStreamReader = new InputStreamReader(
-				inputStream, "UTF-8");
-
-		// Using string builder instead of string concatenation for optimized
-		// performance.
+		// Local variable must be initialized.
+		FileInputStream inputStream = null;
+		InputStreamReader inputStreamReader = null;
 		StringBuilder output = new StringBuilder();
-		int data;
-		while ((data = inputStreamReader.read()) > 0) {
-			if (data < 0x80) {
-				output.append(data);
+
+		try {
+
+			// Reading file as bytes.
+			inputStream = new FileInputStream(this.file);
+
+			// Bridging for easy conversion from byte to character streams with
+			// required encoding.
+			inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+
+			// Using string builder instead of string concatenation for
+			// optimized
+			// performance.
+			int data;
+			while ((data = inputStreamReader.read()) > 0) {
+				if (data < 0x80) {
+					output.append((char) data);
+				}
+			}
+		} catch (IOException e) {
+
+			// Logging needed.
+			// It would be good to handle exception in business layer.
+			// So that appropriate response could be built based on the
+			// exceptions caught.
+			throw e;
+		} finally {
+
+			if (inputStreamReader != null) {
+				inputStreamReader.close();
+			}
+
+			if (inputStream != null) {
+				inputStream.close();
 			}
 		}
-
-		inputStreamReader.close();
-		inputStream.close();
 
 		return output.toString();
 	}
@@ -97,23 +139,47 @@ public class TextFileParser {
 	 * Algorithm refactored for more expressive codes.
 	 */
 	public void saveContent(String content) throws IOException {
-		
-		// Writing file as bytes.
-		FileOutputStream outputStream = new FileOutputStream(file);
 
-		// Bridging for easy conversion from character to byte streams with
-		// required encoding.
-		OutputStreamWriter outputStreamWrither = new OutputStreamWriter(
-				outputStream);
+		// Local variable must be initialized.
+		FileOutputStream outputStream = null;
+		OutputStreamWriter outputStreamWrither = null;
+		BufferedWriter bufferedWriter = null;
 
-		// Chaining character stream writer to buffered writer for the efficient
-		// writing of characters, arrays, and lines.
-		BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWrither);
+		try {
 
-		bufferedWriter.write(content);
+			// Writing file as bytes.
+			outputStream = new FileOutputStream(this.file);
 
-		bufferedWriter.close();
-		outputStreamWrither.close();
-		outputStream.close();
+			// Bridging for easy conversion from character to byte streams with
+			// required encoding.
+			outputStreamWrither = new OutputStreamWriter(outputStream);
+
+			// Chaining character stream writer to buffered writer for the
+			// efficient
+			// writing of characters, arrays, and lines.
+			bufferedWriter = new BufferedWriter(outputStreamWrither);
+
+			bufferedWriter.write(content);
+		} catch (IOException e) {
+
+			// Logging needed.
+			// It would be good to handle exception in business layer.
+			// So that appropriate response could be built based on the
+			// exceptions caught.
+			throw e;
+		} finally {
+
+			if (bufferedWriter != null) {
+				bufferedWriter.close();
+			}
+
+			if (outputStreamWrither != null) {
+				outputStreamWrither.close();
+			}
+
+			if (outputStream != null) {
+				outputStream.close();
+			}
+		}
 	}
 }
