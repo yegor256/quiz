dafddd07b738690915bdc64f5ec8e2e06fae91fe
joraavetisyan <joraavetisyan@gmail.com>
2016-02-02 12:34:01 +0300

    Refactoring Complete
    1. Renamed the class, the name doesn't correspond with the class content.
    2. The class isn't thread safe. I tried to make it thread safe one.
    3. We can avoid function duplication by merging getContent and getContentWithoutUnicode functions.
    4. Used StringBuilder for concatenation strings for making performance more effective.

--- Parser.java
+++ Parser.java
@@ -5,38 +5,41 @@ import java.io.IOException;
 /**
  * This class is thread safe.
  */
-public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+public class FileEditor {
+    private File file;
+    private Object syncObject = new Object();
+    public void setFile(File f) {
+        synchronized (syncObject) {
+            file = f;
+        }
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+    public File getFile() {
+        synchronized (syncObject){
+            return file;
+        }
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+    public String getFileContent(boolean isInUnicode) throws IOException {
+        synchronized (syncObject) {
+            StringBuilder stringBuilder = new StringBuilder();
+            FileInputStream i = new FileInputStream(file);
+            String output = "";
+            int data;
+            while ((data = i.read()) > 0) {
+                if (isInUnicode && data < 0x80) {
+                    stringBuilder.append((char) data);
+                } else {
+                    stringBuilder.append((char) data);
+                }
+            }
+            return stringBuilder.toString();
+        }
     }
-  }
+    public void writeToFile(String str) throws IOException {
+        synchronized (syncObject) {
+            FileOutputStream out = new FileOutputStream(file);
+            for (int i = 0; i < str.length(); i += 1) {
+                out.write(str.charAt(i));
+            }
+        }
+    }
 }
