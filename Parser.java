d0afeec49b36c5e41de0e63c4ef7006cdd68e054
Dalí Freire <dalifreire@gmail.com>
2016-02-02 16:16:33 -0200

    Refactoring the Parser.java class
    1) Added some java docs.
    2) Improvements to make the class more Immutable.
    3) Improvements to make the class more thread safe.
    4) Added general method 'getContent(InputStream inputStream, boolean withoutUnicode)' to provide some code reuse.
    5) Added general method 'saveContent(OutputStream outputStream, String content)' to provide some code reuse.
    6) Parameters validations.
    7) Closes the IntputStream/OutputStream.

--- Parser.java
+++ Parser.java
@@ -2,41 +2,111 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
+ * This class reads and writes an {@link String} content into his associated file. <br/>
  * This class is thread safe.
+ * 
+ * @author Dali Freire - dalifreire@gmail.com
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+    private final File file;
+    private Object lock;
+
+    public Parser(File file) {
+        this.file = file;
+        this.lock = new Object();
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    /**
+     * Returns the associated {@link File} object.
+     * 
+     * @return {@link File}
+     */
+    public File getFile() {
+        return this.file;
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    /**
+     * Reads the {@link InputStream} content.
+     * 
+     * @param inputStream
+     * @param withoutUnicode
+     * @return {@link String}
+     * @throws IOException
+     */
+    private String getContent(InputStream inputStream, boolean withoutUnicode) throws IOException {
+        if (inputStream == null) {
+            throw new IllegalArgumentException("InputStream cannot be null");
+        }
+        synchronized (lock) {
+            try {
+                StringBuffer output = new StringBuffer();
+                int data;
+                while ((data = inputStream.read()) > 0) {
+                    if (!withoutUnicode || data < 0x80) {
+                        output.append((char) data);
+                    }
+                }
+                return output.toString();
+            } finally {
+                inputStream.close();
+            }
+        }
     }
-  }
+
+    /**
+     * Returns the file content into {@link String} format.
+     * 
+     * @return {@link String}
+     * @throws IOException
+     */
+    public String getContent() throws IOException {
+        return getContent(new FileInputStream(file), false);
+    }
+
+    /**
+     * Returns the file content into {@link String} format ignoring unicode characters.
+     * 
+     * @return {@link String}
+     * @throws IOException
+     */
+    public String getContentWithoutUnicode() throws IOException {
+        return getContent(new FileInputStream(file), true);
+    }
+
+    /**
+     * Overwrites the file content by the {@link String} passed as parameter.
+     * 
+     * @param content
+     * @throws IOException
+     */
+    public void saveContent(String content) throws IOException {
+        saveContent(new FileOutputStream(file), content);
+    }
+
+    /**
+     * Overwrites the outputStream content by the {@link String} passed as parameter.
+     * 
+     * @param outputStream
+     * @param content
+     * @throws IOException
+     */
+    private void saveContent(OutputStream outputStream, String content) throws IOException {
+        if (outputStream == null) {
+            throw new IllegalArgumentException("OutputStream cannot be null");
+        }
+        synchronized (lock) {
+            try {
+                for (int i = 0; i < content.length(); i += 1) {
+                    outputStream.write(content.charAt(i));
+                }
+            } finally {
+                outputStream.close();
+            }
+        }
+    }
 }
