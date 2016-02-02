9a1978bc4c687564d9a61aa98e85af9f62e5206e
Artur Lomsadze <lomsadze@bpcbt.com>
2016-02-02 11:03:55 +0300

    fix and improvement

--- Parser.java
+++ Parser.java
@@ -2,41 +2,73 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
   private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
+  private ReadWriteLock rwLock = new ReentrantReadWriteLock();
+
+  public File getFile() {
+    Lock l = rwLock.readLock();
+    File file = null;
+    l.lock();
+    try {
+      file = this.file;
+    } catch (Exception ex) {
+      //...
+    } finally {
+      l.unlock();
+    }
     return file;
   }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+  public void setFile(File file) {
+    Lock l = rwLock.writeLock();
+    l.lock();
+    try {
+      this.file = file;
+    } catch (Exception ex) {
+      //...
+    } finally {
+      l.unlock();
     }
-    return output;
   }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
+
+  public String getContent() throws IOException {
+    StringBuilder output = new StringBuilder();
+    try (FileInputStream i = new FileInputStream(file)) {
+      int data;
+      while ((data = i.read()) > 0) {
+        output.append((char) data);
       }
     }
-    return output;
+    return output.toString();
   }
+
+  public String getContentWithoutUnicode() throws IOException {
+    StringBuilder output = new StringBuilder();
+    try (FileInputStream i = new FileInputStream(file)) {
+      int data;
+      while ((data = i.read()) > 0) {
+        if (data < 0x80) {
+          output.append((char) data);
+        }
+      }
+    }
+    return output.toString();
+  }
+
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+    try (FileOutputStream o = new FileOutputStream(file)) {
+      for (int i = 0; i < content.length(); i += 1) {
+        o.write(content.charAt(i));
+      }
     }
   }
+}
 }
