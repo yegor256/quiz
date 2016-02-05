96f71bbcf881906208a0e6d761bbe6a9b91ab88d
korest <crazyorest@gmail.com>
2016-02-05 20:14:05 +0200

    Thread safe parser.

--- Parser.java
+++ Parser.java
@@ -2,38 +2,61 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Parser {
-  private volatile File file;
-  public void setFile(File f) {
-    file = f;
-  }
-  public File getFile() {
-    return file;
-  }
-  public String getContent(boolean withUnicode) throws IOException {
-	if(file != null) {
-	    try(final FileInputStream i = new FileInputStream(file)) {
-		    StringBuilder output = new StringBuilder();
-		    int data;
-		    while ((data = i.read()) > 0) {
-		    	if (withUnicode || data < 0x80) {
-		    		output.append((char) data);
-		    	}
-		    }
-		    
-		    return output.toString();
-	    }
+
+	private final File file;
+	private final ReentrantReadWriteLock lock;
+
+	public Parser(final File file) {
+		validate(file);
+
+		this.file = file;
+		this.lock = new ReentrantReadWriteLock();
 	}
-	
-	throw new IllegalArgumentException("File is not set");
-  }
-  public String getContentWithoutUnicode() throws IOException {
-	  return getContent(false);
-  }
-  public void saveContent(String content) throws IOException {
-    try(FileOutputStream o = new FileOutputStream(file)) {
-    	o.write(content.getBytes());
-    }
-  }
+
+	public String getContent(final boolean withUnicode) throws IOException {
+		lock.writeLock().lock();
+
+		try (final FileInputStream i = new FileInputStream(file)) {
+			final StringBuilder output = new StringBuilder();
+			int data;
+			while ((data = i.read()) > 0) {
+				if (withUnicode || data < 0x80) {
+					output.append((char) data);
+				}
+			}
+
+			return output.toString();
+		} finally {
+			lock.writeLock().unlock();
+		}
+	}
+
+	public String getContentWithoutUnicode() throws IOException {
+		return getContent(false);
+	}
+
+	public void saveContent(final File file, final String content) throws IOException {
+		lock.readLock().lock();
+
+		try (final FileOutputStream o = new FileOutputStream(file)) {
+			o.write(content.getBytes());
+		} finally {
+			lock.readLock().unlock();
+		}
+	}
+
+	private void validate(final File file) {
+		if (file == null) {
+			throw new NullPointerException("File cannot be null");
+		}
+		if (!file.exists()) {
+			throw new IllegalArgumentException("File not exists");
+		}
+		if (file.isDirectory()) {
+			throw new IllegalArgumentException("File should be file not directory");
+		}
+	}
 }
