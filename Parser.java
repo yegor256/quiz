4fe16f06582b14dffc93a0878757a315d724cd54
Scauser <redscauser@gmail.com>
2016-02-07 00:05:45 +0600

    Update Parser.java
    1. merged methods getContent and getContentWithoutUnicode and created new private method;
    2. added try with resource to close stream at the end;
    
    also for better performance could be used StringBuilder

--- Parser.java
+++ Parser.java
@@ -6,37 +6,70 @@ import java.io.IOException;
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+	private File file;
+	public synchronized void setFile(File f) {
+		file = f;
+	}
+	public synchronized File getFile() {
+		return file;
+	}
+	
+	public String getContent() throws IOException {
+		return getContent(true);
+	}
+	
+	public String getContentWithoutUnicode() throws IOException {
+		return getContent(false);
+	}
+	
+	private String getContent(boolean hasUnicode) throws IOException {
+		FileInputStream i = null;
+		String output = "";
+		try {
+			int data;
+			i = new FileInputStream(file);
+			while ((data = i.read()) > 0) {
+				output += (char) data;
+				if (!hasUnicode || (hasUnicode && data < 0x80)) {
+					output += (char) data;
+				}
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			// finally close input stream to avoid resource leak
+			try {
+				if (i != null) {
+					i.close();
+				}
+			} catch (IOException e) {
+				// ignore
+			}
+		}
+		return output;
+	}
+	public void saveContent(String content) throws IOException {
+		FileOutputStream o = null;
+		try {
+			o = new FileOutputStream(file);
+			for (int i = 0; i < content.length(); i += 1) {
+				o.write(content.charAt(i));
+			}
+			// write all buffered bytes to the file 
+			// and close it to avoid resource leak
+			o.flush();
+			o.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			// finally close output stream to avoid resource leak
+			try {
+				if (o != null) {
+					o.close();
+				}
+			} catch (IOException e) {
+				// ignore
+			}
+		}
+	}
 }
