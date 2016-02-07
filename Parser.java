febf0291d075079c3a2394ffc55b05e77cd24845
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2016-02-07 12:32:14 +0100

    merged code that is the same in getcontent and getcontentwithoutunicode

--- Parser.java
+++ Parser.java
@@ -3,31 +3,14 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-/**
- * This class is thread safe.
- */
 public class Parser {
 
 	public String getContent(File file) throws IOException {
-		FileInputStream i = new FileInputStream(file);
-		String output = "";
-		int data;
-		while ((data = i.read()) > 0) {
-			output += (char) data;
-		}
-		return output;
+		return getContent(file, true);
 	}
 
 	public String getContentWithoutUnicode(File file) throws IOException {
-		FileInputStream i = new FileInputStream(file);
-		String output = "";
-		int data;
-		while ((data = i.read()) > 0) {
-			if (data < 0x80) {
-				output += (char) data;
-			}
-		}
-		return output;
+		return getContent(file, false);
 	}
 
 	public void saveContent(File file, String content) throws IOException {
@@ -35,5 +18,17 @@ public class Parser {
 		for (int i = 0; i < content.length(); i += 1) {
 			o.write(content.charAt(i));
 		}
+	}
+
+	private String getContent(File file, boolean includeUnicode) throws IOException {
+		FileInputStream i = new FileInputStream(file);
+		String output = "";
+		int data;
+		while ((data = i.read()) > 0) {
+			if (includeUnicode || data < 0x80) {
+				output += (char) data;
+			}
+		}
+		return output;
 	}
 }
