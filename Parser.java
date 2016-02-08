64794f08f3968c343ea19b7511af87cc0dbceee8
Alexandr Bondar <ashamansky@gmail.com>
2016-02-08 10:48:31 +0100

    Update Parser.java
    It's should fit single responsobility principle.
    
    So class name Parser means that it should just parse file, but not update it.
    This was the reason why I removed storing method.
    Also this class should be stateless, so we don't need to have local variable File.
    Afterwards this class looks like utility class, so we can declare  methods as 'static'

--- Parser.java
+++ Parser.java
@@ -6,37 +6,22 @@ import java.io.IOException;
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
+
+  public static String parse(File file, Boolean includeUTF) throws IOException {
+    return readFileContent(file, includeUTF);
   }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
+
+  private static String readFileContent(File file, Boolean includeUTF) throws IOException {
     FileInputStream i = new FileInputStream(file);
     String output = "";
     int data;
     while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
+      if (includeUTF) {
+        output += (char) data;
+      } else if(data < 0x80) {
         output += (char) data;
       }
     }
     return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
   }
 }
