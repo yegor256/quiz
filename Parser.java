e6e0bf9634dc8e2de193d10070ad71c7cc41ee7b
Dmitry <hckblues@gmail.com>
2016-02-12 15:51:51 +0200

    Added thread safe implementation with interfaces

--- Parser.java
+++ Parser.java
@@ -1,62 +1,65 @@
-import com.sun.istack.internal.NotNull;
-
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
- * This class is thread safe and immutable.
+ * File parser. Allows to append content to file and read content. This class is mutable and not thread safe.
  */
 public class Parser {
 
-  private final File file;
+	private File file;
 
-  /**
-   * Creates a new parser instance. Best parser in the world that can be useful
-   * for refactoring quiz and parse some content.
-   * @param file file that you probably want to parse.
-   */
-  public Parser(@NotNull File file) {
-    this.file = file;
-  }
+	/**
+	 * Get file content in a string format.
+	 * @return String content of the file.
+	 * @throws java.io.IOException
+	 */
+	public String getContent() throws IOException {
+		try (FileInputStream inputStream = new FileInputStream(file)) {
+			StringBuilder output = new StringBuilder();
+			int data;
+			while ((data = inputStream.read()) > 0) {
+				output.append((char) data);
+			}
+			return output.toString();
+		}
+	}
 
-  /**
-   * Get file content in a string format.
-   *
-   * @return String content of the file.
-   * @throws IOException
-   */
-  public String readContent() throws IOException {
-    try (FileInputStream inputStream = new FileInputStream(file)) {
-      StringBuilder output = new StringBuilder();
-      int data;
-      while ((data = inputStream.read()) > 0) {
-        output.append((char) data);
-      }
-      return output.toString();
-    }
-  }
+	/**
+	 * Get file content without unicode in a string format.
+	 * @return String content of the file.
+	 * @throws java.io.IOException
+	 */
+	public String getContentWithoutUnicode() throws IOException {
+		try (FileInputStream inputStream = new FileInputStream(file)) {
+			StringBuilder output = new StringBuilder();
+			int data;
+			while ((data = inputStream.read()) > 0) {
+				if (data < 0x80) {
+					output.append((char) data);
+				}
+			}
+			return output.toString();
+		}
+	}
 
-  /**
-   * Get file content without unicode in a string format.
-   *
-   * @return String content of the file.
-   * @throws IOException
-   */
-  public String readContentWithoutUnicode() throws IOException {
-    try (FileInputStream inputStream = new FileInputStream(file)) {
-      StringBuilder output = new StringBuilder();
-      int data;
-      while ((data = inputStream.read()) > 0) {
-        if (data < 0x80) {
-          output.append((char) data);
-        }
-      }
-      return output.toString();
-    }
-  }
+	/**
+	 * Write content to the end of the file.
+	 * @param content content to wright
+	 * @throws java.io.IOException
+	 */
+	public void saveContent(String content) throws IOException {
+		try (FileOutputStream o = new FileOutputStream(file)) {
+			o.write(content.getBytes());
+		}
+	}
 
-  public File getFile() {
-    return file;
-  }
+	public void setFile(File file) {
+		this.file = file;
+	}
+
+	public File getFile() {
+		return file;
+	}
 }
