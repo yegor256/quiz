02b9ce82764c2876115a1499a89f4b58e1bbeec2
Moj <MojRoid@users.noreply.github.com>
2016-02-12 22:43:19 +0000

    :sparkles:

--- Parser.java
+++ Parser.java
@@ -2,41 +2,33 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
  */
+
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+    public static String getContent(File file, boolean withUnicode) throws IOException {
+        FileInputStream i = new FileInputStream(file);
+        String output = "";
+        int data;
+        while ((data = i.read()) > 0) {
+            if (withUnicode) {
+                output += (char) data;
+            } else {
+                if (data < 0x80) {
+                    output += (char) data;
+                }
+            }
+        }
+        return output;
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    public static void saveContent(String content, File file) throws IOException {
+        FileOutputStream o = new FileOutputStream(file);
+        for (int i = 0; i < content.length(); i += 1) {
+            o.write(content.charAt(i));
+        }
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
 }
