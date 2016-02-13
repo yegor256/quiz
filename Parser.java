1a55dee7f62627fd9f9fd2825fcea6285fd167fe
Marios Karagiannopoulos <mariosk@gmail.com>
2016-02-13 16:00:33 +0200

    Refactoring
    Refactoring the following stuff:
    
    1) adding synchronized to all methods as we need to implement the class
    as thread safe.
    
    2) adding comments for the throws IOException.
    
    3) use StringBuilder instead of String as it is faster for
    concatenations (garbage collector will be called once instead of String
    that will be called many times and it will be very slow).
    
    4) Use the comparison to != -1 in i.read() as it's not clear what the
    Java API may return. It says explicitly for -1.
    
    5) Adding close statements of the file descriptor. Otherwise we may have
    memory leaking there.
    
    6) write statement is faster to write all the contents at once instead
    of byte by byte.

--- Parser.java
+++ Parser.java
@@ -2,41 +2,65 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.StringBuilder;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
   private File file;
+
   public synchronized void setFile(File f) {
     file = f;
   }
+
   public synchronized File getFile() {
     return file;
   }
-  public String getContent() throws IOException {
+
+  // make sure that the caller would handle the IOException.
+  // Otherwise use a try-catch statement to handle it here.
+  public synchronized String getContent() throws IOException {
     FileInputStream i = new FileInputStream(file);
-    String output = "";
+    // let's use a StringBuilder as it is faster
+    StringBuilder builder = new StringBuilder();
     int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+    while ((data = i.read()) != -1) {
+      builder.append((char)data);
     }
-    return output;
+    i.close();
+    return sb.toString();
   }
-  public String getContentWithoutUnicode() throws IOException {
+
+  // make sure that the caller would handle the IOException.
+  // Otherwise use a try-catch statement to handle it here.
+  public synchronized String getContentWithoutUnicode() throws IOException {
     FileInputStream i = new FileInputStream(file);
-    String output = "";
+    // let's use a StringBuilder as it is faster
+    StringBuilder builder = new StringBuilder();
     int data;
-    while ((data = i.read()) > 0) {
+    while ((data = i.read()) != -1) {
       if (data < 0x80) {
-        output += (char) data;
+        builder.append((char)data);
       }
     }
-    return output;
+    i.close();
+    return sb.toString();
   }
-  public void saveContent(String content) throws IOException {
+
+  // make sure that the caller would handle the IOException.
+  // Otherwise use a try-catch statement to handle it here.
+  public synchronized void saveContent(String content) throws IOException {
     FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+    // if file does not exist, then create it
+    if (!file.exists()) {
+      file.createNewFile();
     }
+    // get the content in bytes
+    byte[] contentInBytes = content.getBytes();
+    o.write(contentInBytes);
+    o.flush();
+    o.close();
   }
+
 }
