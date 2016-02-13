81050d5b27f50f216fbf5f9c4be1fc42377de989
Nikolai90 <nikolaiyatsyk@gmail.com>
2016-02-13 13:38:35 +0200

    Refactoring

--- Parser.java
+++ Parser.java
@@ -5,38 +5,53 @@ import java.io.IOException;
 /**
  * This class is thread safe.
  */
-public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+public class Parser
+{
+	private File file;
+	
+	public synchronized void setFile(File file)
+	{
+		this.file = file;
+	}
+
+	public synchronized File getFile()
+	{
+		return file;
+	}
+	
+	public String getContent() throws IOException
+	{
+		FileInputStream stream = new FileInputStream(file);
+		String output = "";
+		int data;
+		while((data = stream.read()) > 0)
+		{
+			output += (char)data;
+		}
+		return output;
+	}
+	
+	public String getContentWithoutUnicode() throws IOException
+	{
+		FileInputStream stream = new FileInputStream(file);
+		String output = "";
+		int data;
+		while ((data = stream.read()) > 0)
+		{
+			if (data < 0x80)
+			{
+				output += (char) data;
+			}
+		}
+		return output;
+	}
+	
+	public void saveContent(String content) throws IOException
+	{
+		FileOutputStream stream = new FileOutputStream(file);
+		for (int i = 0, len = content.length(); i < len; i++)
+		{
+			stream.write(content.charAt(i));
+		}
+	}
 }
