471d6b63f60a42f197bc21a9642f9752f7ba853e
Vladislav-Kisliy <vladislav.kisliy@gmail.com>
2016-02-14 21:49:39 +0300

    Refactored code

--- Parser.java
+++ Parser.java
@@ -1,42 +1,78 @@
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    private File file;
+
+    public void setFile(File f) {
+        lock.writeLock().lock();
+        try {
+            this.file = f;
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    public File getFile() {
+        lock.readLock().lock();
+        try {
+            return file;
+        } finally {
+            lock.readLock().unlock();
+        }
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    public String getContent() throws IOException {
+        return readFile(false);
     }
-  }
+
+    public String getContentWithoutUnicode() throws IOException {
+        return readFile(true);
+    }
+
+    public String readFile(boolean filterUtf) throws IOException {
+        if (file == null) {
+            throw new IllegalArgumentException("file is null");
+        } else {
+            lock.readLock().lock();
+            try {
+                FileInputStream i = new FileInputStream(file);
+                StringBuilder output = new StringBuilder();
+                int data;
+                while ((data = i.read()) > 0) {
+                    if (filterUtf) {
+                        if (data < 0x80) {
+                            output.append((char) data);
+                        }
+                    } else {
+                        output.append((char) data);
+                    }
+                }
+                return output.toString();
+            } finally {
+                lock.readLock().unlock();
+            }
+        }
+    }
+
+    public void saveContent(String content) throws IOException {
+        lock.writeLock().lock();
+        try {
+            try (Writer fileWriter = new FileWriter(file, false)) {
+                fileWriter.write(content);
+            }
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
 }
+
