c96a85e184f220aab24f98a986f6c6c2ca8e8f9e
Mohamed Maalej <maalejmedti@gmail.com>
2016-02-14 12:11:45 +0100

    Update Parser.java
    - Using BufferedReader  to reduce the costs of read() operation of (InputStreamReader ,FileReader,...) 
    - BufferedReader is the best choice for reading text content and ensuring multi-threading by default ...
    - BufferedWriter is the most suitable for printing String content here (We can user PrintWriter here but BufferedWriter is thread-safe)

--- Parser.java
+++ Parser.java
@@ -1,42 +1,66 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+	private File file;
+
+	public synchronized void setFile(File f) {
+		file = f;
+	}
+
+	public synchronized File getFile() {
+		return file;
+	}
+
+	// Reading a unicode file
+	public String getContent() throws IOException {
+		InputStream fileInputStream = new FileInputStream(file);
+		InputStreamReader reader = new InputStreamReader(fileInputStream,
+				Charset.forName("UTF-8"));
+		BufferedReader brReader = new BufferedReader(reader);
+		String output = "";
+		String data;
+		while ((data = brReader.readLine()) != null) {
+			output += data;
+		}
+		brReader.close();
+		reader.close();
+		fileInputStream.close();
+		return output;
+	}
+
+	// Reading a non-unicode file
+
+	public String getContentWithoutUnicode() throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+
+		String output = "";
+		String line;
+		while ((line = reader.readLine()) != null) {
+
+			output += line;
+		}
+		reader.close();
+		return output;
+	}
+
+	// Saving the file
+	public void saveContent(String content) throws IOException {
+		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+		writer.write(content);
+		writer.close();
+	}
 }
