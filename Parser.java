93b9eb051bcaaaee505fa3d950f27e05ebfab880
Marios Karagiannopoulos <mariosk@gmail.com>
2016-02-16 00:13:14 +0200

    Revert "Revert "Moving from one-thread-at-a-time sync to multiple threads at a time""
    This reverts commit d390469dd1b1733aa763cdd84188ba9f6bb19992.

--- Parser.java
+++ Parser.java
@@ -9,19 +9,26 @@ import java.lang.StringBuilder;
  */
 public class Parser {
   private File file;
+  /*  The lock field is declared private final Object, so that is inaccessible to callers that are outside the class's scope */
+  private final Object lock = new Object();
 
-  public synchronized void setFile(File f) {
-    file = f;
+  public setFile(File f) {
+    synchronized (lock) {
+      this.file = f;
+    }
   }
 
-  public synchronized File getFile() {
-    return file;
+  public File getFile() {
+    return this.file;
   }
 
   // make sure that the caller would handle the IOException.
   // Otherwise use a try-catch statement to handle it here.
-  public synchronized String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+  public String getContent() throws IOException {
+    // let's mark as synchronized only the critical section. Locks on the private object.
+    synchronized (lock) {
+      FileInputStream i = new FileInputStream(file);
+    }
     // let's use a StringBuilder as it is faster
     StringBuilder builder = new StringBuilder();
     int data;
@@ -34,8 +41,11 @@ public class Parser {
 
   // make sure that the caller would handle the IOException.
   // Otherwise use a try-catch statement to handle it here.
-  public synchronized String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+  public String getContentWithoutUnicode() throws IOException {
+    // let's mark as synchronized only the critical section. Locks on the private object.
+    synchronized (lock) {
+      FileInputStream i = new FileInputStream(file);
+    }
     // let's use a StringBuilder as it is faster
     StringBuilder builder = new StringBuilder();
     int data;
@@ -50,11 +60,14 @@ public class Parser {
 
   // make sure that the caller would handle the IOException.
   // Otherwise use a try-catch statement to handle it here.
-  public synchronized void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    // if file does not exist, then create it
-    if (!file.exists()) {
-      file.createNewFile();
+  public void saveContent(String content) throws IOException {
+    // let's mark as synchronized only the critical section. Locks on the private object.
+    synchronized (lock) {
+      // if file does not exist, then create it
+      if (!file.exists()) {
+        file.createNewFile();
+      }
+      FileOutputStream o = new FileOutputStream(file);
     }
     // get the content in bytes
     byte[] contentInBytes = content.getBytes();
