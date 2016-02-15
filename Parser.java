9027a3fae6f91948ef72c5166c0b1a9351f944e2
Taylor Bell <taylorbell.home@gmail.com>
2016-02-15 16:43:29 -0600

    Began refactoring.

--- Parser.java
+++ Parser.java
@@ -1,42 +1,59 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
  */
+
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
+
+  public String getContent(File file) throws IOException {
+
+    FileInputStream inputStream = new FileInputStream(file);
+    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+    StringBuilder builder = new StringBuilder();
+    String inputLine;
+
+    while ((inputLine = reader.readLine()) != null)
+      builder.append(inputLine);
+
+    reader.close();
+
+    return builder.toString();
+
   }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+
+  public String getContentWithoutUnicode(File file) throws IOException {
+
+    FileInputStream inputStream = new FileInputStream(file);
     String output = "";
     int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
+
+    while ((data = inputStream.read()) > 0) {
       if (data < 0x80) {
         output += (char) data;
       }
     }
+
+    inputStream.close();
+
     return output;
+
   }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
+
+  public void saveContent(String content, File outputFile) throws IOException {
+
+    byte[] buffer = content.getBytes(); 
+
+    FileOutputStream outputStream = new FileOutputStream(outputFile);
+    outputStream.write(buffer);
+    outputStream.close();
+
   }
+
 }
