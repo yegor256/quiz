f6c2f51febf515e9beb5d176363270cf58c49590
lukaszprzybylski <przybylski.lukasz@o2.pl>
2016-02-16 01:12:53 +0100

    Refactoring v1
    - added file to location, it was missed
    - deleted from methods get and set synchronized, not implemented correctly
    - FileInputStream is sorrounded by try catch, changed throws IOException to catch properly exceptions
    - changed condition >0 to -1 for inpustream
    - added finally block for closing stream with catching exceptions
    - FileOutputStream is  sorrounded by try catch, changed throws IOException to catch proper exceptions
    - It could be improved by changing file encoding sth like: new FileOutputStream("Your_file_fullpath" ),Charset.forName("UTF8")

--- Parser.java
+++ Parser.java
@@ -1,42 +1,102 @@
+
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+ 
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+    File sdcard = Environment.getExternalStorageDirectory();
+    private File file = new File(sdcard,"example.txt");
+ 
+    public void setFile(File f) {
+        file = f;
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+ 
+    public File getFile() {
+        return file;
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+ 
+    public String getContent() {
+ 
+        FileInputStream i = null;
+        String output = "";
+        try {
+            i = new FileInputStream(file);
+ 
+            int data;
+            try {
+                while ((data = i.read()) != -1) {
+                    output += (char) data;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException fn) {
+            fn.printStackTrace();
+ 
+        } finally {
+            try {
+                if (i != null)
+                    i.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return output;
     }
-  }
+ 
+    public String getContentWithoutUnicode() {
+ 
+        FileInputStream i = null;
+        String output = "";
+        try {
+            i = new FileInputStream(file);
+            int data;
+            while ((data = i.read()) != -1) {
+                if (data < 0x80) {
+                    break;
+                }
+                output += (char) data;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (i != null)
+                    i.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return output;
+    }
+ 
+    public void saveContent(String content) {
+ 
+        FileOutputStream o = null;
+        try {
+            o = new FileOutputStream(file);
+            byte[] contentInBytes = content.getBytes();
+            o.write(contentInBytes);
+            o.flush();
+            o.close();
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+ 
+            try {
+                if (o != null) {
+                    o.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+ 
+            }
+        }
+    }
 }
