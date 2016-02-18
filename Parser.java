f4e3610490718366148365591972733567e51642
Srđan Panić <srdja.panic@gmail.com>
2016-02-18 15:33:54 +0100

    Refactoring the Parser

--- Parser.java
+++ Parser.java
@@ -1,42 +1,64 @@
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+
 /**
- * This class is thread safe.
+ * Parser's read and write operations are thread safe, however, 
+ * different instances of the Parser that are accessing the 
+ * same file will still have to be externally synchronized.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+ 	
+ 	
+	private final File file;
+	
+	
+	public Parser(File file) {
+		this.file = file;
+	}
+	
+	
+	public File getFile() {
+		return file;
+	} 
+
+    /**
+	 * Parses the contents of the file. 
+	 * 
+	 * @param withUnicode if true unicode character will be included, otherwise only ascii
+	 *        characters are returned
+	 *        
+	 * @return parsed file contents
+	 * 
+	 * @throws IOException if the file could not be read
+	 */
+	public synchronized String getContent(boolean withUnicode) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		StringBuilder  sb = new StringBuilder();
+		
+		String line;
+		while ((line = br.readLine()) != null) {
+			sb.append(line);
+		}
+		String content = sb.toString();
+		br.close();
+		
+		return withUnicode ? content.replaceAll("[^\\x00-\\x7f]", "") : content;
+	}
+	
+	/**
+	 * Saves the content to the file
+	 * 
+	 * @param content that is to be saved
+	 * 
+	 * @throws IOException if the contents could not be saved to the file
+	 */
+	public synchronized void saveContent(String content) throws IOException {
+		PrintWriter pw = new PrintWriter(file);
+		pw.print(content);
+		pw.close();
+	}
+
 }
