6b5fa4fe241023bd98a3d7f688f1871e5e43d06e
MrPorterCo <mrporterco@gmail.com>
2016-02-19 22:36:35 -0700

    15 minute refactor
    -Add stringbuilder
    -Add buffer
    -Pass file as param
    -Close streams

--- Parser.java
+++ Parser.java
@@ -1,42 +1,54 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
-/**
- * This class is thread safe.
- */
+
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+
+	public String getContent(File file) throws IOException {
+		StringBuilder output = new StringBuilder();
+		BufferedReader buffReader = new BufferedReader(new FileReader(file));
+		char[] buffer = new char[1024];
+		int len = 0;
+		try {
+			while((len = buffReader.read(buffer)) != -1){
+				output.append(buffer, 0, len);
+			}
+			return output.toString();
+		}
+		finally {
+			buffReader.close();
+		}
+	}
+	public String getContentWithoutUnicode(File file) throws IOException {
+		final FileInputStream i = new FileInputStream(file);
+		try {
+			StringBuilder output = new StringBuilder();
+			int data;
+			while ((data = i.read()) != -1) {
+				if (data < 0x80) {
+					output.append((char) data);
+				}
+			}
+			return output.toString();
+		}
+		finally {
+			i.close();
+		}
+	}
+	public void saveContent(String content, File file) throws IOException {
+
+		final FileOutputStream o = new FileOutputStream(file);
+
+		try {
+			for (int i = 0; i < content.length(); i += 1) {
+				o.write(content.charAt(i));
+			}
+		}
+		finally {
+			o.close();
+		}
+	}
 }
