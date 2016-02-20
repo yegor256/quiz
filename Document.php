654910a5a8b244450d865d0811747d1c690170c1
Anas Salman <asalman@souq.com>
2016-02-21 00:01:06 +0200

    ~ remove redundant code

--- Document.php
+++ Document.php
@@ -1,29 +1,46 @@
 <?php
 class Document {
 
+    const NAME_INDEX = 1;
     const TITLE_INDEX = 3;
     const CONTENT_INDEX = 6;
 
-    public $user;
+    private $user;
+    private $name;
+    private $title;
+    private $content;
 
-    public $name;
-
-    public function init($name, User $user) {
+    public static function getDocument($name, User $user) {
         assert(strlen($name) > 5);
+        $db = Database::getInstance();
+        $row = $db->query('SELECT * FROM document WHERE name = "' . $name . '" LIMIT 1');
+        return new Document($row, $user);
+    }
+
+    private function __construct($row, User $user) {
         $this->user = $user;
-        $this->name = $name;
+        if (empty($row)) {
+            throw new Exception('Empty result set!');
+        }
+        $this->name = $row[self::NAME_INDEX];
+        $this->title = $row[self::TITLE_INDEX];
+        $this->content = $row[self::CONTENT_INDEX];
+    }
+
+    public function getUser() {
+        return $this->user;
+    }
+
+    public function getName() {
+        return $this->name;
     }
 
     public function getTitle() {
-        $db = Database::getInstance();
-        $row = $db->query('SELECT * FROM document WHERE name = "' . $this->name . '" LIMIT 1');
-        return $row[self::TITLE_INDEX]; // third column in a row
+        return $this->title;
     }
 
     public function getContent() {
-        $db = Database::getInstance();
-        $row = $db->query('SELECT * FROM document WHERE name = "' . $this->name . '" LIMIT 1');
-        return $row[self::CONTENT_INDEX]; // sixth column in a row
+        return $this->content;
     }
 
     public static function getAllDocuments() {
@@ -35,8 +52,7 @@ class Document {
 class User {
 
     public function makeNewDocument($name) {
-        $doc = new Document();
-        $doc->init($name, $this);
+        $doc = Document::getDocument($name, $this);
         return $doc;
     }
 
