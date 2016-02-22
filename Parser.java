927d246951aa1d133f42b2287ce6b022d3c207cd
bvanalder <benoit.vanalder@sai-digital.com>
2016-02-22 10:19:24 -0600

    Parser.java refractoring

--- Parser.java
+++ Parser.java
@@ -1,42 +1,53 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+
+	private AtomicReference<File> file = new AtomicReference<>();
+
+	private ReadWriteLock fileLock = new ReentrantReadWriteLock();
+
+	public void setFile(File f) {
+		file.set(f);
+	}
+
+	public File getFile() {
+		return file.get();
+	}
+
+	public String getContent(final boolean onlyAscii) throws IOException {
+		try (final FileInputStream i = new FileInputStream(getFile());) {
+			fileLock.readLock().lock();
+			final StringBuilder stb = new StringBuilder();
+			int data;
+			while ((data = i.read()) > 0) {
+				if (Character.UnicodeBlock.BASIC_LATIN.equals(Character.UnicodeBlock.of((char) data)) 
+						|| onlyAscii) {
+					stb.append((char) data);
+				}
+			}
+			return stb.toString();
+		} finally {
+			fileLock.readLock().unlock();
+		}
+	}
+
+	public void saveContent(final String content) throws IOException {
+		try (final BufferedWriter outputFile = new BufferedWriter(new FileWriter(getFile()));) {
+			fileLock.writeLock().lock();
+			outputFile.write(content);
+		} finally {
+			fileLock.writeLock().unlock();
+		}
+	}
+
 }
