4200840edc886aba663fdd17148b6ed16df38ff5
junaid <junaidp@gmail.com>
2016-02-22 10:34:19 +0500

    Update Parser.java
    change made by junaid

--- Parser.java
+++ Parser.java
@@ -1,3 +1,5 @@
+package ff.server;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -6,37 +8,38 @@ import java.io.IOException;
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+	private File file;
+	private FileInputStream fileInputStream ;
+	String output = "";
+	int data;
+
+	public synchronized void setFile(File file) {
+		this.file = file;
+	}
+	public synchronized File getFile() {
+		return file;
+	}
+	public String getContent(boolean withUniCode) throws IOException {
+		fileInputStream = new FileInputStream(file);
+		while ((data = fileInputStream.read()) > 0) {
+			output += (char) data;
+		}
+		return output;
+	}
+	public String getContentWithoutUnicode() throws IOException {
+		fileInputStream = new FileInputStream(file);
+		while ((data = fileInputStream.read()) > 0) {
+			if (data < 0x80) {
+				output += (char) data;
+			}
+		}
+		return output;
+	}
+	public void saveContent(String content) throws IOException {
+		@SuppressWarnings("resource")
+		FileOutputStream fileOutPutStream = new FileOutputStream(file);
+		for (int i = 0; i < content.length(); i += 1) {
+			fileOutPutStream.write(content.charAt(i));
+		}
+	}
 }
