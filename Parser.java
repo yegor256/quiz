15022c7f6db64b0337e968c4cd03a025fb208d9e
Dragan Dujovski <dujovski@gmail.com>
2016-02-25 09:45:25 +0100

    Added files via upload

--- Parser.java
+++ Parser.java
@@ -7,36 +7,63 @@ import java.io.IOException;
  */
 public class Parser {
   private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+    public synchronized void setFile(File f) {
+        file = f;
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    public synchronized File getFile() {
+        return file;
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    public String getContent() throws IOException {
+        // In this case file can be null
+        if (file == null) {
+            throw new FileNotFoundException();
+        }
+
+        FileInputStream i = new FileInputStream(file);
+        String output = "";
+        int data;
+        while ((data = i.read()) > 0) {
+            output += (char) data;
+        }
+        return output;
     }
-  }
+
+    public String getContentWithoutUnicode() throws IOException {
+        if (file == null) {
+            throw new FileNotFoundException();
+        }
+
+        FileInputStream i = new FileInputStream(file);
+        String output = "";
+        int data;
+        while ((data = i.read()) > 0) {
+            if (data < 0x80) {
+                output += (char) data;
+            }
+        }
+        return output;
+    }
+
+    public void saveContent(String content) throws IOException {
+        // in all cases file can be null
+        if (file == null) {
+            throw new FileNotFoundException();
+        }
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(file);
+            for (int i = 0; i < content.length(); i += 1) {
+                fos.write(content.charAt(i));
+            }
+        } finally {
+            // This is changed because from java standards output streams need to be closed
+            if (fos != null) {
+                fos.close();
+            }
+        }
+    }
 }
