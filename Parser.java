0400e2550eec19309ec0a00660cb82b19cf95759
liudongqing <liudongqing@gmail.com>
2016-02-28 12:48:30 +0800

    Make the Parser immutable; Make resoure safe; Replace string to stringbuffer for performance

--- Parser.java
+++ Parser.java
@@ -7,36 +7,43 @@ import java.io.IOException;
  */
 public class Parser {
   private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
+
+  /**
+   * Make it's a imutabble object.
+   */
+  public Parser(File file){
+	  if(file==null){
+		  throw new IllegalArgumentException("File cannot be Null.");
+	  }
+	  this.file = file;
   }
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+    try(FileInputStream i = new FileInputStream(file)){
+      StringBuffer output = new StringBuffer();
+      int data;
+      while ((data = i.read()) > 0) {
+        output.append((char) data);
+      }
+      return output;
     }
-    return output;
   }
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
+    try(FileInputStream i = new FileInputStream(file)){
+      StringBuffer output = new StringBuffer();
+      int data;
+      while ((data = i.read()) > 0) {
+        if (data < 0x80) {
+          output.append((char) data);
+        }
       }
+      return output;
     }
-    return output;
   }
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+    try(FileOutputStream o = new FileOutputStream(file)){
+      for (int i = 0; i < content.length(); i += 1) {
+        o.write(content.charAt(i));
+      }
     }
   }
 }
