e6012a99348a44e54669db02f823c2bece095e18
Chris Brulak <cbrulak@league.com>
2016-02-28 19:07:43 -0500

    small refactor

--- Parser.java
+++ Parser.java
@@ -4,34 +4,40 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 /**
  * This class is thread safe.
+
+I avoid exceptions like the plauge. Not only do they break program execution but they are expensive: http://stackoverflow.com/a/299315/29505
+
  */
 public class Parser {
   private File file;
+  public final int UNICODE_BOUNDARY = 0x80;
   public synchronized void setFile(File f) {
     file = f;
   }
   public synchronized File getFile() {
     return file;
   }
-  public String getContent() throws IOException {
+  
+   public String getContent() throws IOException 
+   {
+   	   return getContent(-1); 
+   }
+  //not always a big fan of default arguments but since this is a refactor, not a greenfield I would leave in case of backwards compatibility issues
+  public String getContent(int upperBound) throws IOException {
     FileInputStream i = new FileInputStream(file);
     String output = "";
     int data;
     while ((data = i.read()) > 0) {
-      output += (char) data;
+		if((data < upperBound) && (upperBound != -1))
+		{
+      	  output += (char) data;
+	  	}
     }
     return output;
   }
+  
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
+	  return getContent(UNICODE_BOUNDARY);
   }
   public void saveContent(String content) throws IOException {
     FileOutputStream o = new FileOutputStream(file);
