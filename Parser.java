61dc94f978d732fe80a85288376c91d86303c0fd
Christopher Herrera <eefretsoul@gmail.com>
2016-03-01 14:42:22 -0400

    My Approach
    Implementing lock on write, changing to instance use, synchronizing read method that use the File, optimizing reading.

--- Parser.java
+++ Parser.java
@@ -1,42 +1,78 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+package com.company;
+
+import java.io.*;
+import java.nio.channels.FileLock;
+import java.nio.channels.OverlappingFileLockException;
+import java.util.Scanner;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
   private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+  public Parser(File file){
+    if(file == null || !file.exists()){
+        throw new IllegalArgumentException();
     }
-    return output;
+      this.file = file;
   }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
+
+  private synchronized String read(boolean withUnicode) throws java.io.IOException{
+    StringBuilder sb = new StringBuilder();
+    FileInputStream inputStream = null;
+    Scanner sc = null;
+    try {
+      inputStream = new FileInputStream(this.file);
+      sc = new Scanner(inputStream, "UTF-8");
+      while (sc.hasNextLine()) {
+        String line;
+        if(!withUnicode){
+          line = sc.nextLine().replaceAll("\\p{C}", "");
+        }else {
+          line = sc.nextLine();
+        }
+        sb.append(line);
+      }
+      if (sc.ioException() != null) {
+        throw sc.ioException();
+      }
+    } finally {
+      if (inputStream != null) {
+        inputStream.close();
+      }
+      if (sc != null) {
+        sc.close();
       }
     }
-    return output;
+    return sb.toString();
+  }
+
+  public synchronized String getContent() throws IOException {
+    return read(true);
+  }
+  public synchronized String getContentWithoutUnicode() throws IOException {
+    return read(false);
   }
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
+      FileOutputStream fileOutputStream = new FileOutputStream(this.file);
+      boolean written = false;
+      do {
+          try{
+              FileLock lock = fileOutputStream.getChannel().lock();
+              try{
+                  fileOutputStream.write(content.getBytes());
+                  written = true;
+              }finally {
+                  lock.release();
+              }
+          }catch (OverlappingFileLockException e){
+              try{
+                  Thread.sleep(100);
+              }catch (InterruptedException ex){
+                  throw new InterruptedIOException("waiting for lock");
+              }
+          }
+      }while(!written);
   }
 }
