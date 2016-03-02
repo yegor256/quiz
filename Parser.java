d7a46015c1bd82cde988c76ddbd565fc870bf723
liudongqing <liudongqing@gmail.com>
2016-03-02 11:53:17 +0800

    make the io more efficient, make the methods static

--- Parser.java
+++ Parser.java
@@ -1,24 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
 
-  /**
-   * Make it's a imutabble object.
-   */
-  public Parser(File file){
-	  if(file==null){
-		  throw new IllegalArgumentException("File cannot be Null.");
-	  }
-	  this.file = file;
-  }
-  public String getContent() throws IOException {
-    try(FileInputStream i = new FileInputStream(file)){
+  public static String getContent(File file) throws IOException {
+    try(BufferedReader i = new BufferedReader(new FileReader(file))){
       StringBuffer output = new StringBuffer();
       int data;
       while ((data = i.read()) > 0) {
@@ -27,7 +20,7 @@ public class Parser {
       return output;
     }
   }
-  public String getContentWithoutUnicode() throws IOException {
+  public static String getContentWithoutUnicode(File file) throws IOException {
     try(FileInputStream i = new FileInputStream(file)){
       StringBuffer output = new StringBuffer();
       int data;
@@ -36,14 +29,13 @@ public class Parser {
           output.append((char) data);
         }
       }
-      return output;
+      return output.toString();
     }
   }
-  public void saveContent(String content) throws IOException {
-    try(FileOutputStream o = new FileOutputStream(file)){
-      for (int i = 0; i < content.length(); i += 1) {
-        o.write(content.charAt(i));
-      }
+  public static void saveContent(String content, File file) throws IOException {
+    try(BufferedWriter writer = new BufferedWriter(new FileWriter(file))){
+      writer.write(content);
+      writer.flush();
     }
   }
 }
