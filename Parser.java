91c0d730b057e4d88d022f1652e2d1083af938da
Alexander Bashmakov <alexander.bashmakov@intel.com>
2016-03-03 13:03:27 -0700

    1. Added proper whitespace formatting (tabs, newlines) 2. Changed to use StringBuilder in getContent and getContentWithoutUnicode for efficiency 3. Added null checks on 'file' before opening in/out streams 4. Added closing of file in/out streams 5. Synchronized 'saveContent' for thread safety 6. Added FileNotFoundException throws clause in *content* function signatures

--- Parser.java
+++ Parser.java
@@ -1,42 +1,62 @@
+package strategy;
+
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 /**
  * This class is thread safe.
  */
-public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+public class Parser
+{
+	private File file;
+	
+	public synchronized void setFile(File f) {
+		file = f;
+	}
+  
+	public synchronized File getFile() {
+		return file;
+	}
+
+	public String getContent() throws IOException, FileNotFoundException {
+		if (file == null) {
+			return "";
+		}
+		FileInputStream i = new FileInputStream(file);
+		StringBuilder output = new StringBuilder();
+		int data;
+		while ((data = i.read()) > 0) {
+			output.append((char) data);
+		}
+		i.close();
+		return output.toString();
+	}
+
+	public String getContentWithoutUnicode() throws IOException, FileNotFoundException {
+		if (file == null) {
+			return "";
+		}
+		FileInputStream i = new FileInputStream(file);
+		StringBuilder output = new StringBuilder();
+		int data;
+		while ((data = i.read()) > 0) {
+			if (data < 0x80) {
+				output.append((char) data);
+			}
+		}
+		i.close();
+		return output.toString();
+	}
+  
+	public synchronized void saveContent(String content) throws IOException, FileNotFoundException {
+		if (file != null) {
+			FileOutputStream o = new FileOutputStream(file);
+			for (int i = 0; i < content.length(); i += 1) {
+				o.write(content.charAt(i));
+			}
+			o.close();
+		}
+	}
 }
