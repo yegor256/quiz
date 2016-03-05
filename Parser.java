a5e0389e4ae6cde9e0fa7d95bf1b3a9a30dca21f
Aurelian Rosca <aurelian1008@gmail.com>
2016-03-05 11:40:48 +0200

    Remade the class methods.

--- Parser.java
+++ Parser.java
@@ -1,42 +1,46 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+
+	private static final int UNICODE_TEST_CHAR = 0x80;
+
+	public synchronized void saveContentToFile(final String content, final File file) throws FileNotFoundException, IOException {
+		final FileOutputStream o = new FileOutputStream(file);
+
+		for (int i = 0; i < content.length(); i++) {
+			o.write(content.charAt(i));
+		}		  
+		o.close();
+	}
+
+	public synchronized String getContentOfFile(final File file) throws IOException {
+		return getGeneralTheContentContentOfFile(file, false);
+	}
+
+	private String getGeneralTheContentContentOfFile(final File file, boolean isUnicode) throws FileNotFoundException, IOException {
+		FileInputStream i = new FileInputStream(file);
+		String output = "";
+		int data;
+		while ((data = i.read()) > 0) {
+			if (isUnicode) {
+				if (data < UNICODE_TEST_CHAR) {
+					output += (char) data;
+				}
+			} else {
+				output += (char) data;				
+			}
+		}
+		i.close();
+		return output;
+	}
+
+	public synchronized String getContentOfFileWithoutUnicode(final File file) throws FileNotFoundException, IOException {
+		return getGeneralTheContentContentOfFile(file, true);
+	}
 }
