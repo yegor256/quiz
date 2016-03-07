21a7d989491087cbb0d8094c3f04553b8dcbee46
Josh Fischer <josh@joshfischer.io>
2016-03-06 21:29:57 -0600

    clean up

--- Parser.java
+++ Parser.java
@@ -2,41 +2,43 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-/**
- * This class is thread safe.
- */
+
+
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+	private File file;
+
+	public synchronized void setFile(File f) {
+		file = f;
+	}
+
+	public synchronized File getFile() {
+		return file;
+	}
+
+	public String getContent(Boolean withoutUnicode) throws IOException {
+		FileInputStream i = new FileInputStream(file);
+		String output = "";
+		int data;
+		if (withoutUnicode) {
+			
+			while ((data = i.read()) > 0) {
+				if (data < 0x80) {
+					output += (char) data;
+				}
+			}
+		} else {
+			
+			while ((data = i.read()) > 0) {
+				output += (char) data;
+			}
+		}
+		return output;
+	}
+
+	public void saveContent(String content) throws IOException {
+		FileOutputStream o = new FileOutputStream(file);
+		for (int i = 0; i < content.length(); i += 1) {
+			o.write(content.charAt(i));
+		}
+	}
 }
