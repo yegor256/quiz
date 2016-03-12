8a52d5bfda81c85715dded0a3c3f345d039706bf
amit kumar saha <amit.saha012@gmail.com>
2016-03-12 05:02:31 -0600

    15 min refactoring
    1. Added constructor
    2. Used BufferedReader & BufferedWriter to increase efficiency for
    reading and writing .
    3. Closed stream objects.
    4. Removed unnecessary usage of synchronized.
    5.  Used BufferedReader for better string memory allocation.

--- Parser.java
+++ Parser.java
@@ -1,42 +1,59 @@
+package com.amit.parser;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 /**
  * This class is thread safe.
  */
 public class Parser {
   private File file;
-  public synchronized void setFile(File f) {
-    file = f;
+  public Parser(File file){
+	  if(file==null){
+		  throw new IllegalArgumentException("file can not be null");
+	  }
+	  this.file = file;
   }
-  public synchronized File getFile() {
+  public synchronized void setFile(File f) {
+    this.file = f;
+  }
+  public File getFile() {
     return file;
   }
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+    StringBuilder output = new StringBuilder();
+    BufferedReader br = new BufferedReader(new FileReader(this.file));
+    String line=null;
+    while ((line = br.readLine()) != null) {
+       output.append(line);
     }
-    return output;
+    br.close();
+    return output.toString();
   }
   public String getContentWithoutUnicode() throws IOException {
     FileInputStream i = new FileInputStream(file);
-    String output = "";
+    StringBuilder output = new StringBuilder();
     int data;
     while ((data = i.read()) > 0) {
       if (data < 0x80) {
-        output += (char) data;
+    	  output.append((char) data);
       }
     }
-    return output;
+    i.close();
+    return output.toString();
   }
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
+    
+    if (!this.file.exists()) {
+		this.file.createNewFile();
+	}
+	FileWriter fw = new FileWriter(file.getAbsoluteFile());
+	BufferedWriter bw = new BufferedWriter(fw);
+	bw.write(content);
+	bw.close();
   }
 }
