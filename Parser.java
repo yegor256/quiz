d8cd9bef0cfcad44b84091277282a47d316c5185
Alex <brodjag@mail.ru>
2016-03-14 14:14:12 +0300

    Alex corrections
    1)i.read() returns  -1 to detect end.
    2)unicode has 2 byte from 0x00 to 0xff but it consist ASCII simbols <=+007F
    3).write(byte[]) more smarty.
    4)Stream should be close  after usage.

--- Parser.java
+++ Parser.java
@@ -14,29 +14,31 @@ public class Parser {
     return file;
   }
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+    FileInputStream i = new FileInputStream(getFile());
     String output = "";
     int data;
-    while ((data = i.read()) > 0) {
+    while ((data = i.read()) != -1) {
       output += (char) data;
     }
+    i.close()
     return output;
   }
+  
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+    FileInputStream i = new FileInputStream(getFile());
     String output = "";
     int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
+    while ((data = i.read()) !=-1) {
+      if (data <= 0x007f) {
         output += (char) data;
       }
     }
+    i.close();
     return output;
   }
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
+    FileOutputStream o = new FileOutputStream(getFile());
+    o.write(content.getBytes));
+    o.close()
   }
 }
