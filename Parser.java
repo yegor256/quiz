45cfa8c65cb08779a257eb75be8d1f1ab7068cd0
Vladimir Vedernikov <vvedernikov@accesssoftek.com>
2016-03-15 13:08:01 +0300

    Constructor

--- Parser.java
+++ Parser.java
@@ -7,8 +7,8 @@ import java.io.IOException;
  */
 public class Parser {
   private File file;
-  public synchronized void setFile(File f) {
-    file = f;
+  public Parser(File file) {
+    this.file = file;
   }
   public synchronized File getFile() {
     return file;
