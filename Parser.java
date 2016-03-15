7822f99a60bba8595241c8ff2a991d49333c0652
javiersantos <javier.david.santos@gmail.com>
2016-03-14 22:05:19 -0500

    fixing and improve the class.
    Change use string by stringBuilder to get better performance at string
    handler.
    
    The streams (ImputStream and ouputStream was closed to free memory.
    
    Used to flush method after write at stream (as best practice)

--- Parser.java
+++ Parser.java
@@ -14,29 +14,31 @@ public class Parser {
     return file;
   }
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
+    FileInputStream i = new FileInputStream(getFile());
+    StringBuilder output = new StringBuilder();
     int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+    while ((data = i.read()) != -1) {
+      output.append(data);
     }
-    return output;
+    i.close();
+    return output.toString();
   }
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
+    FileInputStream i = new FileInputStream(getFile());
+    StringBuilder output = new StringBuilder();
     int data;
-    while ((data = i.read()) > 0) {
+    while ((data = i.read()) != -1) {
       if (data < 0x80) {
-        output += (char) data;
+        output.append(data);
       }
     }
-    return output;
+    i.close();
+    return output.toString();
   }
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
+    FileOutputStream o = new FileOutputStream(getFile());
+    o.write(content.getBytes());
+    o.flush();
+    o.close();
   }
 }
