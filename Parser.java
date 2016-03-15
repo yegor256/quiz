9cea835d366d79a1509e5d536eb112bec2dd7042
Nabin Khadka <nbnnbnkhadka@gmail.com>
2016-03-15 08:36:19 +0545

    Refactored Field name according to Java standard

--- Parser.java
+++ Parser.java
@@ -6,15 +6,15 @@ import java.io.IOException;
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
+  private File mFile;
   public synchronized void setFile(File f) {
-    file = f;
+    mFile = f;
   }
   public synchronized File getFile() {
-    return file;
+    return mFile;
   }
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+    FileInputStream i = new FileInputStream(mFile);
     String output = "";
     int data;
     while ((data = i.read()) > 0) {
@@ -23,7 +23,7 @@ public class Parser {
     return output;
   }
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+    FileInputStream i = new FileInputStream(mFile);
     String output = "";
     int data;
     while ((data = i.read()) > 0) {
@@ -34,7 +34,7 @@ public class Parser {
     return output;
   }
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
+    FileOutputStream o = new FileOutputStream(mFile);
     for (int i = 0; i < content.length(); i += 1) {
       o.write(content.charAt(i));
     }
