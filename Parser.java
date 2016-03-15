ede6738060a99f2ef5b53cb7209d194a86e76cca
Vladimir Vedernikov <vvedernikov@accesssoftek.com>
2016-03-15 14:12:29 +0300

    Parser optimization

--- Parser.java
+++ Parser.java
@@ -1,42 +1,35 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public Parser(File file) {
-    this.file = file;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+    private final static String LINE_DELIMITER = "\n";
+    private File file;
+
+    public Parser(File file) {
+        this.file = file;
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    public synchronized File getFile() {
+        return file;
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    public synchronized CharSequence getContent() throws IOException {
+        StringBuilder output = new StringBuilder();
+        Files.lines(file.toPath()).forEachOrdered(line -> output.append(line).append(LINE_DELIMITER));
+        return output;
     }
-  }
+
+    public synchronized CharSequence getContentWithoutUnicode() throws IOException {
+        return getContent().toString().replaceAll("[^\\x00-\\x7F]", "");
+    }
+
+    public synchronized void saveContent(String content) throws IOException {
+        try (FileOutputStream o = new FileOutputStream(file)) {
+            o.write(content.getBytes());
+        }
+    }
 }
