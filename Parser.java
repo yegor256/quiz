7b4834d17aa932132f58eae94bd4811de0e360ef
dsemenov <sdo.semenov@gmail.com>
2016-03-15 23:50:32 -0400

    Fix issues

--- Parser.java
+++ Parser.java
@@ -1,42 +1,68 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
   private File file;
+
   public synchronized void setFile(File f) {
     file = f;
   }
+
   public synchronized File getFile() {
     return file;
   }
+
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
+    return getContentInternal(new AlwaysTruePredicate<Integer>());
   }
+
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+    return getContentInternal(new UnicodePredicate());
+  }
+
+  private synchronized String getContentInternal(Predicate<Integer> predicate) throws IOException {
+    File currentFile = file;
+    if(!currentFile.exists()){
+      //FIXME: clarify requirements for non existing file
+      return "";
+    }
+    FileInputStream i = new FileInputStream(currentFile);
     String output = "";
     int data;
     while ((data = i.read()) > 0) {
-      if (data < 0x80) {
+      if (predicate.apply(data)) {
         output += (char) data;
       }
     }
     return output;
   }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+  public synchronized void saveContent(String content) throws IOException {
+    File currentFile = file;
+    FileOutputStream o = new FileOutputStream(currentFile);
+    try(Writer fileWriter = new BufferedWriter(new OutputStreamWriter(o))){
+      fileWriter.write(content);
     }
   }
+
+  private interface Predicate<TYPE> {
+    boolean apply(TYPE argument);
+  }
+
+  private static class AlwaysTruePredicate<TYPE> implements Parser.Predicate<TYPE> {
+    @Override
+    public boolean apply(TYPE argument) {
+      return true;
+    }
+  }
+
+  private static class UnicodePredicate implements Parser.Predicate<Integer> {
+    @Override
+    public boolean apply(Integer argument) {
+      return argument < 0x80;
+    }
+  }
+
 }
