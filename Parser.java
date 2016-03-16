f00409fda4c00f2b8c979c2bd6fede64d8e009c8
duychuongvn <huynhduychuong@gmail.com>
2016-03-16 11:04:44 +0700

    #36821: duychuongvn.  Using BufferedReader and BufferedOutputStream to minimize accessing the underlying operating system. Using readline() to minimize processing bytes and characters individually. Using constructor to initialize file to avoid changing file

--- Parser.java
+++ Parser.java
@@ -1,42 +1,95 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
   private File file;
-  public synchronized void setFile(File f) {
-    file = f;
+
+  public Parser(File file) {
+    this.file = file;
   }
+
   public synchronized File getFile() {
     return file;
   }
+
+  private FileOutputStream getFileOutputStream() throws IOException {
+    synchronized (this) {
+      checkFileType();
+      return new FileOutputStream(getFile());
+    }
+  }
+
+  private FileReader getFileReader() throws IOException {
+    synchronized (this) {
+      checkFileType();
+      return new FileReader(getFile());
+    }
+  }
+
+  private void checkFileType() throws IOException {
+    if (!file.isFile()) {
+      throw new IOException("File [" + file.getAbsolutePath() + "] not valid");
+    }
+  }
+
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+    FileReader fileReader = getFileReader();
+    BufferedReader bufferedReader = new BufferedReader(fileReader);
+    StringBuilder contentBuilder = new StringBuilder();
     String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
+    try {
+      boolean isNotFirstLine = false;
+      while ((output = bufferedReader.readLine()) != null) {
+        if (isNotFirstLine) {
+          contentBuilder.append("\n");
+        }
+        contentBuilder.append(output);
+        isNotFirstLine = true;
       }
+    } catch (IOException ioException) {
+      throw ioException;
+    } finally {
+      bufferedReader.close();
+      fileReader.close();
     }
-    return output;
+    return contentBuilder.toString();
   }
+
+  public String getContentWithoutUnicode() throws IOException {
+    FileReader fileReader = getFileReader();
+    BufferedReader bufferedReader = new BufferedReader(fileReader);
+    StringBuilder contentBuilder = new StringBuilder();
+    int data;
+    try {
+      while ((data = bufferedReader.read()) > 0) {
+        if (data < 0x80) {
+          contentBuilder.append((char) data);
+        }
+      }
+    } catch (IOException ioException) {
+      throw ioException;
+    } finally {
+      bufferedReader.close();
+      fileReader.close();
+    }
+
+    return contentBuilder.toString();
+  }
+
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+    FileOutputStream fileOutputStream = getFileOutputStream();
+    BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream, 1024);
+
+    try {
+      bufferedOutputStream.write(content.getBytes());
+    } catch (IOException ioException) {
+      throw ioException;
+    } finally {
+      bufferedOutputStream.close();
+      fileOutputStream.close();
     }
   }
+
 }
