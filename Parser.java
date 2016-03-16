58bcefaaf2b8a2dbe128b010781525a5b530c201
time2die <time2rfc@gmail.com>
2016-03-17 00:01:29 +0300

    rewrite read\write code

--- Parser.java
+++ Parser.java
@@ -1,7 +1,10 @@
+import lombok.Cleanup;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
@@ -27,31 +30,74 @@ public class Parser {
     }
 
     public String getContent() throws IOException {
-        FileInputStream i = new FileInputStream(file);
-        String output = "";
+        @Cleanup FileInputStream i = new FileInputStream(file);
+        StringBuilder output = "";
         int data;
         while ((data = i.read()) > 0) {
-            output += (char) data;
+            output.append(()char)data);
         }
         return output;
+    }
+
+    public String getContent() throws IOException {
+        lock.readLock().lock();
+        try (
+                FileInputStream inputStream = new FileInputStream(file)) {
+            StringBuilder outputBuilder = new StringBuilder();
+            int data;
+            byte[] buffer = new byte[1024];
+            while ((data = inputStream.read(buffer)) != -1) {
+                outputBuilder.append(new String(buffer, 0, data).toCharArray());
+            }
+            return outputBuilder.toString();
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public String getContentWithoutUnicode() throws IOException {
-        FileInputStream i = new FileInputStream(file);
-        String output = "";
-        int data;
-        while ((data = i.read()) > 0) {
-            if (data < 0x80) {
-                output += (char) data;
+        lock.readLock().lock();
+        try (
+                FileInputStream inputStream = new FileInputStream(file)) {
+            StringBuilder stringBuffer = StringBuilder();
+            int data;
+            byte[] buffer = new byte[1024];
+            Scanner sc = new Scanner(stringBuffer);
+            while (sc.hasNextLine()) {
+                stringBuffer.append(sc.nextLine());
             }
+            return stringBuffer.toString();
+        } finally {
+            lock.readLock().unlock();
         }
-        return output;
+    }
+
+    public String getContentWithoutUnicode() throws IOException {
+
+        lock.readLock().lock();
+        try (
+                FileInputStream inputStream = new FileInputStream(file)) {
+            StringBuffer stringBuffer = "";
+            int data;
+            while ((data = inputStream.read()) > 0) {
+                if (data < 0x80) {
+                    stringBuffer.append((char) data);
+                }
+            }
+            return output;
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public void saveContent(String content) throws IOException {
-        FileOutputStream o = new FileOutputStream(file);
-        for (int i = 0; i < content.length(); i += 1) {
-            o.write(content.charAt(i));
+        lock.writeLock().lock();
+        try (FileOutputStream o = new FileOutputStream(file)) {
+            for (int i = 0; i < content.length(); i += 1) {
+                o.write(content.getBytes());
+            }
+        } finally {
+            lock.writeLock().unlock();
         }
     }
 }
