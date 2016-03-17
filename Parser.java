2c49adf880c15124fcf55d40819d4df54aebc318
Benson.Zhang <zlhades@outlook.com>
2016-03-17 19:53:55 +0800

    why not remove saveContent,  saveContent method is strange in Parser class.

--- Parser.java
+++ Parser.java
@@ -2,41 +2,63 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+    private static final String EMPTY_STRING = "";
+    private File file;
+
+    public Parser(File file) {
+
+        this.file = file;
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    public synchronized String getContent() throws IOException {
+
+        FileInputStream fileInputStream = null;
+        StringBuilder output = new StringBuilder(EMPTY_STRING);
+        try {
+            fileInputStream = new FileInputStream(file);
+
+            int data;
+            while ((data = fileInputStream.read()) > 0) {
+                output.append((char) data);
+            }
+        } finally {
+            if (fileInputStream != null) {
+                fileInputStream.close();
+            }
+        }
+        return output.toString();
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    public String getContentWithoutUnicode() throws IOException {
+
+        String content = this.getContent();
+        StringBuilder output = new StringBuilder(EMPTY_STRING);
+        for (char c : content.toCharArray()) {
+            if (c < 0x80) {
+                output.append(c);
+            }
+        }
+        return output.toString();
     }
-  }
+
+    public synchronized void saveContent(String content) throws IOException {
+
+        FileOutputStream fileOutputStream = null;
+        try {
+
+            fileOutputStream = new FileOutputStream(file);
+            for (int i = 0; i < content.length(); i += 1) {
+                fileOutputStream.write(content.charAt(i));
+            }
+        } finally {
+            if (fileOutputStream != null) {
+                fileOutputStream.close();
+            }
+        }
+    }
 }
