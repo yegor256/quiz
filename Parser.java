84c4f452b149a062366794bb2ecef94a434582ba
omid <omidpourhadi@gmail.com>
2016-03-17 12:21:13 +0330

    Best Pull Request Ever
    * null is bad
    * don't write code in constructor
    * immutability
    * catch me
    * fail-fast
    * object thinking, Are you still debugging

--- Parser.java
+++ Parser.java
@@ -1,42 +1,139 @@
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
- * This class is thread safe.
+ * @author OmidPourhadi [AT] gmail [DOT] com
+ *
  */
-public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+public final class Parser
+{
+
+    // Immutability
+    private final File file;
+
+    private final ParserDecorator parserDecorator;
+
+    public Parser(File file, ParserDecorator parserDecorator)
+    {
+        // don't write code here
+        this.file = file;
+        this.parserDecorator = parserDecorator;
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    // catch-me exception
+    public String parse() throws IOException
+    {
+        // fail-fast
+        if (file == null)
+            throw new IllegalArgumentException("file can not be null");
+        if (parserDecorator == null)
+            throw new IllegalArgumentException("parserDecorator can not be null");
+        return parserDecorator.parse(file);
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    public void saveContent(String content) throws IOException
+    {
+        Path f = Paths.get(this.file.toURI());
+        Files.write(f, new Words(content), Charset.forName("UTF-8"));
     }
-  }
+
+    public interface ParserDecorator
+    {
+        public String parse(File file) throws UnsupportedEncodingException, IOException;
+    }
+
+    public static class Content implements ParserDecorator
+    {
+        @Override
+        public String parse(File file) throws UnsupportedEncodingException, IOException
+        {
+            // Null is bad
+            String output = "";
+            Words words = new Words(new Text(file).toString());
+            for (String item : words)
+            {
+                output += item;
+            }
+            return output;
+        }
+    }
+
+    public static class ContentWithoutUnicode implements ParserDecorator
+    {
+
+        @Override
+        public String parse(File file) throws UnsupportedEncodingException, IOException
+        {
+            // Null is bad
+            String output = "";
+            Words words = new Words(new Text(file).toString());
+            for (String item : words)
+            {
+                if (new BigInteger(1, item.getBytes()).intValue() < 0x80)
+                {
+                    output += item;
+                }
+            }
+            return output;
+        }
+
+    }
+
+    // Object Thinking
+    private static class Words implements Iterable<String>
+    {
+
+        private String texts;
+
+        Words(String texts)
+        {
+            this.texts = texts;
+        }
+
+        @Override
+        public Iterator<String> iterator()
+        {
+            List<String> words = new ArrayList<String>();
+            for (int i = 0; i < texts.length(); i++)
+            {
+                char c = texts.charAt(i);
+                words.add(Character.toString(c));
+            }
+            return words.iterator();
+        }
+
+    }
+
+    static class Text
+    {
+        private final File f;
+
+        Text(File f)
+        {
+            this.f = f;
+        }
+
+        @Override
+        public String toString()
+        {
+            try
+            {
+                return new String(Files.readAllBytes(Paths.get(f.toURI())), "UTF-8");
+            }
+            catch (IOException e)
+            {
+                throw new IllegalArgumentException("can not read file content", e);
+            }
+        }
+    }
+
 }
