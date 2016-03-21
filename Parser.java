58f09c50b3a059f273e733177ae2a7f14100c23b
Nickolay Laptev <laptev@transct.ru>
2016-03-21 20:14:49 +0300

    Changes: 1. Moved common logic to helper method. 2. Replaced read and write by char with buffered solution with proper closing of resources. 3. Made getContent* and saveContent methods synchronized to guard "file" field access. Without that there is no guarantee actual file value is available. 4. Fixed class comment to mark the class as not thread safe.
    Further issues:
    1. Although "file" field access is synchronized in this class we expose the field in getter. Also we do not share the lock. That means the user of this class cannot synchronize access to this file properly. The same issue is actual for children of Parser class as it's not final.
    There is no quick fix in isolation as Parser class is public along with its methods, so we may break the code that uses it.
    2. Reading unicode by single symbol is not good as it supports characters that hold several symbols. We should use Java API instead and not read content by single character.

--- Parser.java
+++ Parser.java
@@ -1,42 +1,49 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+
 /**
- * This class is thread safe.
+ * This class is not thread safe.
  */
 public class Parser {
+  private static final int UNICODE_MARKER = 0x80;
   private File file;
+
   public synchronized void setFile(File f) {
     file = f;
   }
+
   public synchronized File getFile() {
     return file;
   }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
+
+  public synchronized String getContent() throws IOException {
+    return getContent(file, true);
   }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+
+  private static String getContent(File file, boolean readUnicode) throws IOException {
     String output = "";
     int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
+
+    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+      while ((data = reader.read()) > 0) {
+        if (readUnicode || isNotUnicode(data)) {
+          output += (char) data;
+        }
       }
+      return output;
     }
-    return output;
   }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+  private static boolean isNotUnicode(int charCode) {
+    return charCode < UNICODE_MARKER;
+  }
+
+  public synchronized String getContentWithoutUnicode() throws IOException {
+    return getContent(file, false);
+  }
+
+  public synchronized void saveContent(String content) throws IOException {
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+      writer.write(content);
     }
   }
 }
