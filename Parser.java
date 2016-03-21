4cc65ac1df0d82abd5550bcbc2de40d411981f7c
radoslavlozanov <radoslav.lozanov@gmail.com>
2016-03-21 14:13:46 +0200

    Modifications on Reentrant synchronization.

--- Parser.java
+++ Parser.java
@@ -1,62 +1,97 @@
 import java.io.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * This class is thread safe.
  */
 public class Parser {
-    private File file;
+    public static final int UNICODE_THRESHOLD = 0x80;
 
-    public synchronized void setFile(File f) {
-        file = f;
-    }
+    private String fileName;
+    private ReentrantReadWriteLock fileLocker;
 
-    public synchronized File getFile() {
-        return file;
-    }
+    public Parser(String fileName) {
+        super();
 
-    public synchronized String getContent() throws IOException {
-        return getContent(true);
-    }
-
-    public synchronized String getContentWithoutUnicode() throws IOException {
-        return getContent(false);
-    }
-
-    public synchronized void saveContent(String content) throws IOException {
-        FileWriter fw = new FileWriter(file);
-        fw.write(content);
-        fw.flush();
-        fw.close();
-    }
-
-    private String getContent(boolean includeUnicode) throws IOException {
-        FileInputStream input = new FileInputStream(file);
-        StringBuilder output = new StringBuilder();
-
-        byte[] data = new byte[1024];
-        int read = input.read(data);
-
-        while (read > 0) {
-
-            if (!includeUnicode) {
-                byte[] newData = new byte[read];
-
-                int newRead = 0;
-                for (int i = 0; i < read; i++) {
-                    if (data[i] < 0x80) {
-                        newData[newRead++] = data[i];
-                    }
-                }
-
-                data = newData;
-                read = newRead;
-            }
-
-            output.append(new String(data, 0, read));
-            read = input.read(data);
+        if (fileName == null) {
+            throw new IllegalArgumentException("fileName cannot be null");
         }
 
-        input.close();
+        this.fileName = fileName;
+        this.fileLocker = new ReentrantReadWriteLock();
+    }
+
+    public String getFileName() {
+        return this.fileName;
+    }
+
+    public String getContent() {
+        FileInputStream input = null;
+        StringBuilder output = new StringBuilder();
+        Lock readLock = this.fileLocker.readLock();
+
+        try {
+            readLock.lock();
+            input = new FileInputStream(this.fileName);
+            byte[] data = new byte[1024];
+            int read = input.read(data);
+
+            while (read > 0) {
+                output.append(new String(data, 0, read));
+                read = input.read(data);
+            }
+        } catch (IOException ioe) {
+            System.out.println("File read error");
+            return null;
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {/* do nothing */}
+            }
+            readLock.unlock();
+        }
+
         return output.toString();
+    }
+
+    public String getContentWithoutUnicode() {
+        String content = this.getContent();
+
+        if (content == null) {
+            return null;
+        }
+
+        StringBuilder resultBuilder = new StringBuilder();
+        for (char c : content.toCharArray()) {
+            if (c < UNICODE_THRESHOLD) {
+                resultBuilder.append(c);
+            }
+        }
+
+        return resultBuilder.toString();
+    }
+
+    public boolean saveContent(String content) {
+        Lock writeLock = this.fileLocker.writeLock();
+        FileWriter fw = null;
+
+        try {
+            writeLock.lock();
+            fw = new FileWriter(fileName);
+            fw.write(content);
+            fw.flush();
+        } catch (IOException ioe) {
+            System.out.println("Error wrting to the file");
+            return false;
+        } finally {
+            try {
+                fw.close();
+            } catch (IOException e) {/* Do nothing */}
+            writeLock.unlock();
+        }
+
+        return true;
     }
 }
