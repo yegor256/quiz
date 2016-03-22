03f10c9b491c3c59d67fab8417907b958d436e9b
Paulo Roberto Brandão <betobrandao@gmail.com>
2016-03-21 21:02:52 -0300

    Solving quiz in Java
    Fixing race condition issue when accessing files.
    Closing the stream after use.

--- Parser.java
+++ Parser.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
  */
@@ -10,33 +11,56 @@ public class Parser {
   public synchronized void setFile(File f) {
     file = f;
   }
+
   public synchronized File getFile() {
     return file;
   }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+  
+  public synchronized String getContent() throws IOException {
+    FileInputStream i = null;
     String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
+    
+    try {
+      i =   new FileInputStream(file);
+      int data;
+      while ((data = i.read()) > 0) {
+	output += (char) data;
       }
+    } finally {
+      if( i != null)
+	i.close();
     }
     return output;
   }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+  public synchronized String getContentWithoutUnicode() throws IOException {
+    FileInputStream i = null;
+    String output = "";
+    try {
+      i = new FileInputStream(file);
+      int data;
+      while ((data = i.read()) > 0) {
+	if ((data & 0x80) != 0) {
+	  output += (char) data;
+	}
+      }
+    } finally {
+      if( i != null )
+	i.close();
+    return output;
+    }
+  }
+  
+  public synchronized void saveContent(String content) throws IOException {
+    FileOutputStream o = null;
+    try {
+      o = new FileOutputStream(file);
+      for (int i = 0; i < content.length(); i += 1) {
+	o.write(content.charAt(i));
+      }
+    } finally {
+      if( o != null )
+	o.close();
     }
   }
 }
