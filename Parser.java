1f6bd875b987eb753b8667e2af8b1afe2d097ca5
dyorgio <dyorgio@gmail.com>
2016-03-28 10:24:53 -0300

    Refactoring

--- Parser.java
+++ Parser.java
@@ -1,42 +1,72 @@
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+    // 32Kb buffer size
+    private static final int BUFFER_SIZE = 32*1024;
+
+    // Final object
+    private final File file;
+
+    // init File object on constructor
+    public Parser(String path) {
+        file = new File(path);
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    // specific charset
+    public String getContent(String charset) throws IOException {
+        // syncronize file object
+        synchronized (file) {
+            // auto close resources
+            try (FileInputStream i = new FileInputStream(file)) {
+                // use StringBuilder
+                StringBuilder output = new StringBuilder();
+                // use buffers
+                byte[] data = new byte[BUFFER_SIZE];
+                int readed;
+                while ((readed = i.read(data)) != -1) {
+                    output.append(new String(data, 0, readed, charset));
+                }
+                return output.toString();
+            }
+        }
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    public String getContentWithoutUnicode() throws IOException {
+         // syncronize file object
+        synchronized (file) {
+            // auto close resources
+            try (FileInputStream i = new FileInputStream(file)) {
+                // use StringBuilder
+                StringBuilder output = new StringBuilder();
+                int data;
+                while ((data = i.read()) != -1) {
+                    if (data < 0x80) {
+                        output.append((char) data);
+                    }
+                }
+                return output.toString();
+            }
+        }
     }
-  }
+
+    // specific charset
+    public void saveContent(String content, String charset) throws IOException {
+        // syncronize file object
+        synchronized (file) {
+            // auto close resources
+            try (FileOutputStream o = new FileOutputStream(file)) {
+                // write with specific charset
+                o.write(content.getBytes(charset));
+                // flush data before close
+                o.flush();
+            }
+        }
+    }
 }
