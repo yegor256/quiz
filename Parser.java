e98613ef31fc49c690eb6e71c294cd74b32ddfcd
Rui Castro <rui.castro@gmail.com>
2016-03-31 15:46:04 +0100

    Refactored Parser class

--- Parser.java
+++ Parser.java
@@ -1,42 +1,60 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+    private File file;
+
+    public Parser(File f) {
+        this.file = f;
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    public File getFile() {
+        return file;
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    public String getContent() throws IOException {
+        return getFilteredContent(null);
     }
-  }
+
+    public String getContentWithoutUnicode() throws IOException {
+        return getFilteredContent(new UnicodeFilter());
+    }
+
+    public synchronized void saveContent(String content) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(this.file));
+        writer.write(content);
+    }
+
+    public synchronized String getFilteredContent(Filter f) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(this.file));
+        StringBuilder builder = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (f != null) {
+                line = f.filter(line);
+            }
+            builder.append(line);
+        }
+        return builder.toString();
+    }
+
+    public interface Filter {
+        String filter(String s);
+    }
+
+    class UnicodeFilter implements Filter {
+        public String filter(String s) {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c < 0x80) {
+                    builder.append(c);
+                }
+            }
+            return builder.toString();
+        }
+    }
 }
