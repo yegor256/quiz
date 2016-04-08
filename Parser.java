6a6a422b4977584184ee52270bda467b59a41003
aevseev <evseev.alexei@gmail.com>
2016-04-08 15:31:06 +0600

    Refactoring

--- Parser.java
+++ Parser.java
@@ -1,42 +1,53 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
+  private final File file;
+
+  public Parser(final File file) {
+    validateFile(file);
+    this.file = file;
   }
-  public synchronized File getFile() {
-    return file;
+
+  private void validateFile(final File file) {
+    if (file == null)
+      throw new IllegalArgumentException("File can't be null");
+
+    if (!file.exists())
+      throw new IllegalArgumentException("File does not not exists");
+
+    if (!file.isFile())
+      throw new IllegalArgumentException("File must be normal file");
   }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
+
+  public synchronized String getContent() throws IOException {
+    return getContent(false);
+  }
+
+  public synchronized String getContentWithoutUnicode() throws IOException {
+    return getContent(true);
+  }
+
+  private String getContent(boolean withoutUnicode) throws IOException {
+    StringBuilder sb = new StringBuilder();
     int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
+
+    try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+      while ((data = br.read()) > 0) {
+        if (!withoutUnicode || data < 0x80) {
+          sb.append((char) data);
+        }
       }
     }
-    return output;
+
+    return sb.toString();
   }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+  public synchronized void saveContent(String content) throws IOException {
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+      writer.write(content);
     }
   }
 }
