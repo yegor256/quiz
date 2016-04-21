071102ee47f388b4faf6680eb286f0c31f7ba246
Alan Evans <thealanevans@googlemail.com>
2016-04-21 19:42:15 +0100

    Try with resources

--- Parser.java
+++ Parser.java
@@ -18,31 +18,34 @@ public class Parser {
     }
 
     public String getContent() throws IOException {
-        FileInputStream i = new FileInputStream(file);
-        String output = "";
-        int data;
-        while ((data = i.read()) > 0) {
-            output += (char) data;
+        try (FileInputStream i = new FileInputStream(file)) {
+            String output = "";
+            int data;
+            while ((data = i.read()) > 0) {
+                output += (char) data;
+            }
+            return output;
         }
-        return output;
     }
 
     public String getContentWithoutUnicode() throws IOException {
-        FileInputStream i = new FileInputStream(file);
-        String output = "";
-        int data;
-        while ((data = i.read()) > 0) {
-            if (data < 0x80) {
-                output += (char) data;
+        try (FileInputStream i = new FileInputStream(file)) {
+            String output = "";
+            int data;
+            while ((data = i.read()) > 0) {
+                if (data < 0x80) {
+                    output += (char) data;
+                }
             }
+            return output;
         }
-        return output;
     }
 
     public void saveContent(String content) throws IOException {
-        FileOutputStream o = new FileOutputStream(file);
-        for (int i = 0; i < content.length(); i += 1) {
-            o.write(content.charAt(i));
+        try (FileOutputStream o = new FileOutputStream(file)) {
+            for (int i = 0; i < content.length(); i += 1) {
+                o.write(content.charAt(i));
+            }
         }
     }
 }
