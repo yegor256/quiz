e758e64f22d4f457a3a8b07d091ebcf63b6bae31
Alex Wilson <a@ax.gy>
2016-04-26 01:17:59 +0100

    Initial clean-up

--- php/Document.php
+++ php/Document.php
@@ -1,29 +1,77 @@
 <?php
+
 class Document {
 
+    /**
+     * User of Document.
+     *
+     * @var User
+     */
     public $user;
 
+    /**
+     * Name of Document.
+     *
+     * @var string
+     */
     public $name;
 
-    public function init($name, User $user) {
-        assert(strlen($name) > 5);
+    /**
+     * Constructor
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->db = Database::getInstance();
+    }
+
+    /**
+     * Instantiates a new Document with a given name and User.
+     *
+     * @param  string $name Name.
+     * @param  User   $user User
+     *
+     * @return void
+     */
+    public function init($name, User $user)
+    {
+        if (!strlen($name) > 5)) {
+            throw new Exception("Name is too short.");
+        };
         $this->user = $user;
         $this->name = $name;
     }
 
-    public function getTitle() {
-        $db = Database::getInstance();
-        $row = $db->query('SELECT * FROM document WHERE name = "' . $this->name . '" LIMIT 1');
+    /**
+     * Title of the current Document.
+     *
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        $row = $this->db->query('SELECT * FROM document WHERE name = "' . $this->name . '" LIMIT 1');
         return $row[3]; // third column in a row
     }
 
-    public function getContent() {
-        $db = Database::getInstance();
-        $row = $db->query('SELECT * FROM document WHERE name = "' . $this->name . '" LIMIT 1');
+    /**
+     * Content of the current Document.
+     *
+     * @return string
+     */
+    public function getContent(): string
+    {
+        $row = $this->db->query('SELECT * FROM document WHERE name = "' . $this->name . '" LIMIT 1');
         return $row[6]; // sixth column in a row
     }
 
-    public static function getAllDocuments() {
+    /**
+     * Returns all Documents.
+     * 
+     * @return Document[]
+     */
+    public static function getAllDocuments(): array
+    {
         // to be implemented later
     }
 
--- php/User.php
+++ php/User.php
@@ -1,19 +1,28 @@
 <?php
+
 class User {
 
-    public function makeNewDocument($name) {
+    /**
+     * Creates a new Document with a given name.
+     *
+     * @param  string $name Name for the new Document.
+     *
+     * @return Document
+     */
+    public function makeNewDocument($name): Document {
         $doc = new Document();
         $doc->init($name, $this);
         return $doc;
     }
 
-    public function getMyDocuments() {
-        $list = array();
-        foreach (Document::getAllDocuments() as $doc) {
-            if ($doc->user == $this)
-                $list[] = $doc;
-        }
-        return $list;
+    /**
+     * Returns all documents for the current user.
+     *
+     * @return Document[]
+     */
+    public function getMyDocuments(): array {
+        array_filter(Document::getAllDocuments(), function($document) {
+            return ($doc->user === $this);
+        });
     }
-
 }
