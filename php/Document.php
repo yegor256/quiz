8ed124413929a0ae45f852d805d0ceb1d3dee84a
Alex Wilson <a@ax.gy>
2016-04-26 01:09:59 +0100

    Move classes.

--- php/Document.php
+++ php/Document.php
@@ -28,22 +28,3 @@ class Document {
     }
 
 }
-
-class User {
-
-    public function makeNewDocument($name) {
-        $doc = new Document();
-        $doc->init($name, $this);
-        return $doc;
-    }
-
-    public function getMyDocuments() {
-        $list = array();
-        foreach (Document::getAllDocuments() as $doc) {
-            if ($doc->user == $this)
-                $list[] = $doc;
-        }
-        return $list;
-    }
-
-}
