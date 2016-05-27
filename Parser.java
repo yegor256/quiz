c0723bf4753e246232905c34086cd57bffaad932
ingvord <tla-ingvord@yandex.ru>
2016-05-27 08:25:15 +0300

    refactor Parser

--- Parser.java
+++ Parser.java
@@ -1,42 +1,56 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+
 /**
- * This class is thread safe.
+ * This class is thread safe, i.e. only one thread can read or write from/to wrapped file.
  */
+//TODO add java concurrency annotations to the project
+//@ThreadSafe
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+    private final File file;
+
+    public Parser(File file) {
+        this.file = file;
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+
+    //TODO without any knowledge on how this class is actually used, e.g. its load, it does not make sense to do anything more than synchronized
+    public synchronized String getContent() throws IOException {
+        StringBuilder output = new StringBuilder();
+        try (BufferedInputStream i = newBufferedInputStream()) {
+            //TODO read by large chunks
+            int data;
+            while ((data = i.read()) != -1) {
+                output.append((char) data);
+            }
+            return output.toString();
+        }
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    public synchronized String getContentWithoutUnicode() throws IOException {
+        StringBuilder output = new StringBuilder();
+        try(BufferedInputStream i = newBufferedInputStream()) {
+            int data;
+            //TODO read by large chunks
+            while ((data = i.read()) > 0) {
+                if (data < 0x80) {
+                    output.append((char) data);
+                }
+            }
+            return output.toString();
+        }
     }
-  }
+
+    private BufferedInputStream newBufferedInputStream() throws FileNotFoundException{
+        return new BufferedInputStream(new FileInputStream(file));
+    }
+
+    private BufferedOutputStream newBufferedOutputStream() throws FileNotFoundException {
+        return new BufferedOutputStream(new FileOutputStream(file));
+    }
+
+    public synchronized void saveContent(String content) throws IOException {
+        try(BufferedOutputStream o = newBufferedOutputStream()) {
+            o.write(content.getBytes());
+        }
+    }
 }
