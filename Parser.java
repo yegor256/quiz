da3c9fa290a9d1d07da3fc239211d776713431a1
mac <mocanu.cristian93@gmail.com>
2016-06-04 23:19:42 +0300

    Use clearer and more consistent naming scheme

--- Parser.java
+++ Parser.java
@@ -9,40 +9,40 @@ import java.io.IOException;
 public class Parser {
     private File file;
 
-    public synchronized void setFile(File f) {
-        file = f;
+    public synchronized void setFile(File file) {
+        this.file = file;
     }
 
     public synchronized File getFile() {
-        return file;
+        return this.file;
     }
 
     public String getContent() throws IOException {
-        FileInputStream i = new FileInputStream(file);
-        String output = "";
+        FileInputStream input = new FileInputStream(file);
+        String content = "";
         int data;
-        while ((data = i.read()) > 0) {
-            output += (char) data;
+        while ((data = input.read()) > 0) {
+            content += (char) data;
         }
-        return output;
+        return content;
     }
 
     public String getContentWithoutUnicode() throws IOException {
-        FileInputStream i = new FileInputStream(file);
-        String output = "";
+        FileInputStream input = new FileInputStream(file);
+        String content = "";
         int data;
-        while ((data = i.read()) > 0) {
+        while ((data = input.read()) > 0) {
             if (data < 0x80) {
-                output += (char) data;
+                content += (char) data;
             }
         }
-        return output;
+        return content;
     }
 
     public void saveContent(String content) throws IOException {
-        FileOutputStream o = new FileOutputStream(file);
+        FileOutputStream output = new FileOutputStream(file);
         for (int i = 0; i < content.length(); i += 1) {
-            o.write(content.charAt(i));
+            output.write(content.charAt(i));
         }
     }
 }
