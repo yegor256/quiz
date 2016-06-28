cf0a327da9e6de62c8291cc7f69ac1b0ce1c7534
dhilipsiva <dhilipsiva@gmail.com>
2016-06-28 18:31:45 +0530

    Refactor `getContent` method

--- Parser.java
+++ Parser.java
@@ -7,34 +7,36 @@ import java.io.IOException;
  */
 public class Parser {
   private File file;
+
   public synchronized void setFile(File f) {
     file = f;
   }
+
   public synchronized File getFile() {
     return file;
   }
-  public String getContent() throws IOException {
+
+  public String getContent(boolean withUnicode) throws IOException {
     FileInputStream fileInputStream = new FileInputStream(file);
     String output = "";
     int data;
     while ((data = fileInputStream.read()) > 0) {
-      output += (char) data;
+	  if (withUnicode || data < 0x80){
+      	output += (char) data;
+	  }
     }
 	fileInputStream.close();
     return output;
   }
+
+  public String getContent() throws IOException{
+	return getContent(true);
+  }
+
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream fileInputStream = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = fileInputStream.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-	fileInputStream.close();
-    return output;
+	return getContent(false);
   }
+
   public void saveContent(String content) throws IOException {
     FileOutputStream fileOutputStream = new FileOutputStream(file);
 	byte contentBytes[] = content.getBytes();
