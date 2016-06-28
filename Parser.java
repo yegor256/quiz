ea1708cd8416d7bd6a6425df762610e45931359c
dhilipsiva <dhilipsiva@gmail.com>
2016-06-28 18:23:27 +0530

    Refactor Variables, close streams ...
    ... & Refactor saveContent method

--- Parser.java
+++ Parser.java
@@ -14,29 +14,31 @@ public class Parser {
     return file;
   }
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+    FileInputStream fileInputStream = new FileInputStream(file);
     String output = "";
     int data;
-    while ((data = i.read()) > 0) {
+    while ((data = fileInputStream.read()) > 0) {
       output += (char) data;
     }
+	fileInputStream.close();
     return output;
   }
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+    FileInputStream fileInputStream = new FileInputStream(file);
     String output = "";
     int data;
-    while ((data = i.read()) > 0) {
+    while ((data = fileInputStream.read()) > 0) {
       if (data < 0x80) {
         output += (char) data;
       }
     }
+	fileInputStream.close();
     return output;
   }
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
+    FileOutputStream fileOutputStream = new FileOutputStream(file);
+	byte contentBytes[] = content.getBytes();
+	fileOutputStream.write(contentBytes);
+	fileOutputStream.close();
   }
 }
