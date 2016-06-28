9f6d7c15e4794639193ea68572bc362eb58e8ec1
dhilipsiva <dhilipsiva@gmail.com>
2016-06-28 18:35:55 +0530

    Add Doc Strings

--- Parser.java
+++ Parser.java
@@ -9,14 +9,23 @@ public class Parser {
   private File file;
 
   public synchronized void setFile(File f) {
+	/*
+	 * Thread-safe file setter
+	 */
     file = f;
   }
 
   public synchronized File getFile() {
+	/*
+	 * Thread-safe file getter
+	 */
     return file;
   }
 
   public String getContent(boolean withUnicode) throws IOException {
+	/*
+	 * Read the contents of file and return it as string with or without unicode
+	 */
     FileInputStream fileInputStream = new FileInputStream(file);
     String output = "";
     int data;
@@ -30,14 +39,23 @@ public class Parser {
   }
 
   public String getContent() throws IOException{
+	/*
+	 * Read contents of file with unicodes and return it as string
+	 */
 	return getContent(true);
   }
 
   public String getContentWithoutUnicode() throws IOException {
+	/*
+	 * Read contents of file without unicodes and return it as string
+	 */
 	return getContent(false);
   }
 
   public void saveContent(String content) throws IOException {
+	/*
+	 * Save the String content to the file
+	 */
     FileOutputStream fileOutputStream = new FileOutputStream(file);
 	byte contentBytes[] = content.getBytes();
 	fileOutputStream.write(contentBytes);
