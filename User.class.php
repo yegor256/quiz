11c41864985951af35f1dff602b3c32bc6ccc1ae
Matheus Falcão <theusx@gmail.com>
2016-08-04 09:34:48 -0300

    Fix indent and created a single file by class

---
+++ User.class.php
@@ -0,0 +1,19 @@
+
+<?php
+class User {
+
+  public function makeNewDocument($name) {
+    $doc = new Document();
+    $doc->init($name, $this);
+    return $doc;
+  }
+
+  public function getMyDocuments() {
+    $list = array();
+    foreach (Document::getAllDocuments() as $doc) {
+      if ($doc->user == $this) $list[] = $doc;
+    }
+    return $list;
+  }
+
+}
