2a61a1d7ad0c21b6074b1f9bd16d2d75a19e553d
Bay Batu <batuhanbayrakci@gmail.com>
2016-08-06 11:53:14 +0300

    use stringbuilder instead of string class for string concatenation in loop

--- Parser.java
+++ Parser.java
@@ -24,14 +24,14 @@ public class Parser {
   }
   public String getContentWithoutUnicode() throws IOException {
     FileInputStream i = new FileInputStream(file);
-    String output = "";
+    StringBuilder output = new StringBuilder();
     int data;
     while ((data = i.read()) > 0) {
       if (data < 0x80) {
-        output += (char) data;
+        output.append((char) data);
       }
     }
-    return output;
+    return output.toString();
   }
   public void saveContent(String content) throws IOException {
     FileOutputStream o = new FileOutputStream(file);
