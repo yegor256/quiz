c9e62f7a539a1d6f89b6142e0308f82414adfeb3
rexim <reximkut@gmail.com>
2016-08-24 23:14:19 +0600

    Autoclose acquired resource via try-with-resources

--- Parser.java
+++ Parser.java
@@ -13,31 +13,38 @@ public class Parser {
   }
 
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
     String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+    try (FileInputStream i = new FileInputStream(file)) {
+      int data;
+      while ((data = i.read()) > 0) {
+        output += (char) data;
+      }
     }
+
     return output;
   }
 
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
     String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
+
+    try (FileInputStream i = new FileInputStream(file)) {
+      int data;
+      while ((data = i.read()) > 0) {
+        if (data < 0x80) {
+          output += (char) data;
+        }
       }
     }
+
     return output;
   }
 
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+    try (FileOutputStream o = new FileOutputStream(file)) {
+      for (int i = 0; i < content.length(); i += 1) {
+        o.write(content.charAt(i));
+      }
     }
   }
 }
