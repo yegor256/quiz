227ea9254720f6d73e88cc8925ea73e15063a173
Sadegh Kazemy <Sadegh.Kazemy@gmail.com>
2016-08-30 21:53:38 +0300

    Add some line breaks.

--- Parser.java
+++ Parser.java
@@ -2,17 +2,22 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
+
   private File file;
+
   public synchronized void setFile(File f) {
     file = f;
   }
+
   public synchronized File getFile() {
     return file;
   }
+
   public String getContent() throws IOException {
     FileInputStream i = new FileInputStream(file);
     String output = "";
@@ -22,6 +27,7 @@ public class Parser {
     }
     return output;
   }
+
   public String getContentWithoutUnicode() throws IOException {
     FileInputStream i = new FileInputStream(file);
     String output = "";
@@ -33,6 +39,7 @@ public class Parser {
     }
     return output;
   }
+
   public void saveContent(String content) throws IOException {
     FileOutputStream o = new FileOutputStream(file);
     for (int i = 0; i < content.length(); i += 1) {
