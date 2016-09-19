93bb0f0efb1b4d957355eab755e047e8c7390b25
Swapnil Marghade <s.g.marghade@gmail.com>
2016-09-19 17:04:04 +0530

    Refactoring for Parser.java

--- Parser.java
+++ Parser.java
@@ -6,11 +6,13 @@ import java.io.IOException;
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
+  private final File file;
+
+  public Parser(File file) {
+      this.file = file;
   }
-  public synchronized File getFile() {
+
+  public File getFile() {
     return file;
   }
   public String getContent() throws IOException {
@@ -20,6 +22,7 @@ public class Parser {
     while ((data = i.read()) > 0) {
       output += (char) data;
     }
+    i.close();
     return output;
   }
   public String getContentWithoutUnicode() throws IOException {
@@ -31,12 +34,14 @@ public class Parser {
         output += (char) data;
       }
     }
+    i.close();
     return output;
   }
-  public void saveContent(String content) throws IOException {
+  public synchronized void saveContent(String content) throws IOException {
     FileOutputStream o = new FileOutputStream(file);
     for (int i = 0; i < content.length(); i += 1) {
       o.write(content.charAt(i));
     }
+    o.close();
   }
 }
