86874a04a3dc5f83e12f34886eb4c69d8ea0dd50
williansmartins <professorwillians@gmail.com>
2016-09-29 15:22:13 -0300

    Changing type immutable to mutable, thread safe, etc

--- Parser.java
+++ Parser.java
@@ -15,23 +15,23 @@ public class Parser {
   }
   public String getContent() throws IOException {
     FileInputStream i = new FileInputStream(file);
-    String output = "";
+    StringBuffer output = new StringBuffer();
     int data;
     while ((data = i.read()) > 0) {
-      output += (char) data;
+      output.append((char) data);
     }
-    return output;
+    return output.toString();
   }
   public String getContentWithoutUnicode() throws IOException {
     FileInputStream i = new FileInputStream(file);
-    String output = "";
+    StringBuffer output = new StringBuffer();
     int data;
     while ((data = i.read()) > 0) {
       if (data < 0x80) {
-        output += (char) data;
+        output.append((char) data);
       }
     }
-    return output;
+    return output.toString();
   }
   public void saveContent(String content) throws IOException {
     FileOutputStream o = new FileOutputStream(file);
