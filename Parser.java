be0dda51628c74aca93f267e91490e055bc19996
lechuckcaptain <lechuckcaptain@gmail.com>
2016-10-07 18:25:16 +0200

    Added constructor with final file parameter Updated and synchronized content methods

--- Parser.java
+++ Parser.java
@@ -1,42 +1,33 @@
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+    private final File _file;
+
+    public Parser(File file)
+    {
+        _file = file;
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    public String getContent() throws IOException {
+        synchronized (_file) {
+            return new String(Files.readAllBytes(_file.toPath()));
+        }
     }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+    public String getContentWithoutUnicode() throws IOException {
+        return getContent().replaceAll("\\P{InBasic_Latin}", "");
     }
-  }
+
+    public void saveContent(String content) throws IOException {
+        synchronized (_file) {
+            Files.write(_file.toPath(), content.getBytes(), StandardOpenOption.WRITE);
+        }
+    }
 }
