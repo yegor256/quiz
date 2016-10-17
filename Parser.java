eb63a7b34b73e406dcff9f267183408efc3e5041
Michael Brown <mbrown@incipire.com>
2016-10-17 01:21:35 -0500

    Initial refactoring pass.

--- Parser.java
+++ Parser.java
@@ -2,26 +2,30 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
   private File file;
+  //I would remove the setter/getter for file and make it a parameter to the constructor.
+  //If a user needs to change the file, it should require a new parser.
   public synchronized void setFile(File f) {
     file = f;
   }
   public synchronized File getFile() {
     return file;
   }
+
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
+    byte[] bytes = Files.readAllBytes(file.getPath());
+    return new String(bytes);
   }
+  //TODO: implement a FilteredInputStream that takes a lambda for which characters to skip
+  //this would allow alternate cases to be easily implemented and use that stream to read
+  //the character. Also would use a stringbuffer instead of string concat (more performant)
   public String getContentWithoutUnicode() throws IOException {
     FileInputStream i = new FileInputStream(file);
     String output = "";
@@ -34,9 +38,7 @@ public class Parser {
     return output;
   }
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
+      PrintWriter writer = new PrintWriter(file);
+      writer.print(content);
   }
 }
