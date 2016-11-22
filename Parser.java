72f03436f548a5975fdeb74cc429a954ce533a5b
Marius Gligor <marius.gligor@archive360.com>
2016-11-22 19:21:48 +0200

    refactoring to immutable class

--- Parser.java
+++ Parser.java
@@ -1,42 +1,55 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+
 /**
  * This class is thread safe.
+ * This class is now immutable (read only) and obviously is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
+  private final File file;
+
+  public Parser(File f) {
     file = f;
   }
-  public synchronized File getFile() {
+
+  public File getFile() {
     return file;
   }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
+
+  public String getContent() {
+      try (BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"))) {
+          StringBuilder output = new StringBuilder();
+          int data;
+          while ((data = in.read()) > 0) {
+              output.append((char) data);
+          }
+          return output.toString();
+      } catch (IOException e) {
+          throw new RuntimeException(e);
       }
-    }
-    return output;
   }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
+
+  public String getContentWithoutUnicode() {
+      try (BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"))) {
+          StringBuilder output = new StringBuilder();
+          int data;
+          while ((data = in.read()) > 0) {
+              if (data < 0x80) {
+                  output.append((char) data);
+              }
+          }
+          return output.toString();
+      } catch (IOException e) {
+          throw new RuntimeException(e);
+      }
+  }
+
+  public void saveContent(String content) {
+      if (content != null) {
+          try (FileOutputStream o = new FileOutputStream(file)) {
+              o.write(content.getBytes("UTF-8"), 0, content.length());
+          } catch (IOException e) {
+              throw new RuntimeException(e);
+          }
+      }
   }
 }
