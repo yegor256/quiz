0c81d815aaa6819dd6c45453590e08a918f1d580
artem <ayrapetovai@yandex.ru>
2021-03-07 13:31:37 +0300

    refactor

--- Parser.java
+++ Parser.java
@@ -1,46 +1,59 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
+  private final int BUFFER_SIZE = 4096;
+  private final File file;
+  private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+  public Parser(File file) {
+    this.file = file;
   }
-  public synchronized File getFile() {
+  public File getFile() {
     return file;
   }
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
+    return read(false);
   }
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
+    return read(true);
   }
-  public void saveContent(String content) {
-    FileOutputStream o = new FileOutputStream(file);
-    try {
-      for (int i = 0; i < content.length(); i += 1) {
-        o.write(content.charAt(i));
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+  public String read(final boolean withoutUnicode) throws IOException {
+    readWriteLock.readLock().lock();
+    try (BufferedReader reader = new BufferedReader(new FileReader((file)))) {
+      StringBuilder sb = new StringBuilder(file.length() < Integer.MAX_VALUE? (int) file.length(): Integer.MAX_VALUE);
+      char[] readBuffer = new char[BUFFER_SIZE];
+      int sbIndex = 0;
+      int read;
+      do {
+        read = reader.read(readBuffer);
+        if (read > 0) {
+          if (withoutUnicode) {
+            for (int i = 0; i < read; i++) {
+              char c = readBuffer[i];
+              if (c < 0x80) {
+                sb.insert(sbIndex++, c);
+              }
+            }
+          } else {
+            sb.append(readBuffer, 0, read);
+          }
+        }
+      } while (read > 0);
+      return sb.toString();
+    } finally {
+      readWriteLock.readLock().unlock();
+    }
+  }
+  public void saveContent(String content) throws IOException {
+    readWriteLock.writeLock().lock();
+    try (FileOutputStream o = new FileOutputStream(file)) {
+      o.write(content.getBytes(StandardCharsets.UTF_8));
+    } finally {
+      readWriteLock.writeLock().unlock();
     }
   }
 }
