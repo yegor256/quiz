ef61bfc2b4bdd45f87fd205376bbbf9c6b823dcc
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-04-16 10:37:31 +0500

    Fix example

--- Program.java
+++ Program.java
@@ -2,15 +2,13 @@ import java.io.File;
 import java.io.IOException;
 
 public class Program implements Runnable {
-    private final String[] args;
     private final TextSource source;
     private final Storage storage;
 
     public Program(final String[] args) {
-        this.args = args;
         final File destination = new File("test.txt");
         this.source = new UnicodeFreeTextSource(new FileTextSource(destination));
-        this.storage = new FileTextStorage(destination, this.source);
+        this.storage = new FileTextStorage(destination, args[0]);
     }
 
     @Override
